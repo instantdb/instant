53a85349f6d154a5e40423f05cb7b6b72a90ab9d
Joe Averbukh <joeaverbukh@gmail.com>
2024-08-27 10:26:12 -0700

    Add split attributes example

--- client/sandbox/react-nextjs/pages/patterns/split-attributes.tsx
+++ client/sandbox/react-nextjs/pages/patterns/split-attributes.tsx
@@ -1,28 +1,50 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import { init, tx, id } from "@instantdb/react";
 import config from "../../config";
-import Login from "../../components/Login";
 
 const { transact, useQuery } = init(config);
 
+function addFoo(text: string, privateText: string) {
+  const privateFooId = id();
+  transact([
+    // First we create the private data
+    tx.privateFoos[privateFooId].update({ privateText }),
+
+    // And then we create the public data and link it with private
+    tx.foos[id()].update({ text }).link({ privateFoos: privateFooId })
+  ])
+}
+
+function deleteFoos(foos: any) {
+  transact(foos.map((foo: any) => tx.foos[foo.id].delete()));
+}
+
 function App() {
-  const { isLoading, error, data } = useQuery({ todos: { todosPrivate: {} } });
+  const { isLoading, error, data } = useQuery({ foos: { privateFoos: {} } });
+  const [text, setText] = useState("");
+  const [privateText, setPrivateText] = useState("");
   if (isLoading) { return <div>Loading...</div>; }
   if (error) { return <div>Uh oh! {error.message}</div>; }
 
-  const { todos } = data;
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    addFoo(text, privateText);
+  }
+
+  const { foos } = data;
   return (
-    <div className="mx-w-md mx-auto p-4">
+    <div className="mx-w-md mx-auto p-4 flex flex-col w-1/2 space-y-2">
       <div className="font-bold">Todos</div>
-      {todos.map(({ id, text }) => {
-        return (
-          <div key={id}>
-            <p>{text}</p>
-          </div>
-        );
-      })}
-      {/* TODO: Finish this example */}
+      <form className="space-y-2 flex flex-col" onSubmit={handleSubmit}>
+        <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="public text..." />
+        <input type="text" value={privateText} onChange={(e) => setPrivateText(e.target.value)} placeholder="private text..." />
+        <button className="py-2 border-2 border-black" type="submit">Add foo!</button>
+      </form>
+      <button className="py-2 border-2 border-black" onClick={() => deleteFoos(foos)}>Delete all</button>
+      <div className="w-sm">
+        <pre>{JSON.stringify(foos, null, 2)}</pre>
+      </div>
     </div>
   )
 }
