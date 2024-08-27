5ec8a24e0add735277dcb43fad2c52f3b45ee01c
Marky @ Instant <marky@instantdb.com>
2024-08-27 15:05:53 -0700

    oop

--- client/sandbox/cli-nodejs/instant.perms.ts
+++ client/sandbox/cli-nodejs/instant.perms.ts
@@ -1,38 +1,29 @@
 export default {
-  "tags": {
-    "bind": [
-      "isOwner",
-      "auth.id in data.ref('posts.authors.userId')"
-    ],
-    "allow": {
-      "view": "true",
-      "create": "isOwner",
-      "delete": "isOwner",
-      "update": "isOwner"
-    }
+  authors: {
+    bind: ["isAuthor", "auth.id == data.userId"],
+    allow: {
+      view: "true",
+      create: "isAuthor",
+      update: "isAuthor",
+      delete: "isAuthor",
+    },
   },
-  "posts": {
-    "bind": [
-      "isAuthor",
-      "auth.id in data.ref('authors.userId')"
-    ],
-    "allow": {
-      "view": "true",
-      "create": "isAuthor",
-      "delete": "isAuthor",
-      "update": "isAuthor"
-    }
+  posts: {
+    bind: ["isAuthor", "auth.id in data.ref('authors.userId')"],
+    allow: {
+      view: "true",
+      create: "isAuthor",
+      update: "isAuthor",
+      delete: "isAuthor",
+    },
   },
-  "authors": {
-    "bind": [
-      "isAuthor",
-      "auth.id == data.userId"
-    ],
-    "allow": {
-      "view": "true",
-      "create": "isAuthor",
-      "delete": "isAuthor",
-      "update": "isAuthor"
-    }
-  }
+  tags: {
+    bind: ["isOwner", "auth.id in data.ref('posts.authors.userId')"],
+    allow: {
+      view: "true",
+      create: "isOwner",
+      update: "isOwner",
+      delete: "isOwner",
+    },
+  },
 };
--- client/sandbox/cli-nodejs/instant.schema.ts
+++ client/sandbox/cli-nodejs/instant.schema.ts
@@ -1,51 +1,49 @@
-// moo
-// http://localhost:3000/dash?s=main&t=home&app=fd6db6f5-aad8-44df-aad9-e49ed8668967
+import "dotenv/config";
 
 import { i } from "@instantdb/core";
 
-const INSTANT_APP_ID = "fd6db6f5-aad8-44df-aad9-e49ed8668967";
-
+// Example entities and links (you can delete these!)
 const graph = i.graph(
-  INSTANT_APP_ID,
+  process.env.INSTANT_APP_ID!,
   {
-    "authors": i.entity({
-      "name": i.any(),
-      "userId": i.any(),
+    posts: i.entity({
+      name: i.string(),
+      content: i.string(),
     }),
-    "posts": i.entity({
-      "content": i.any(),
-      "name": i.any(),
+    authors: i.entity({
+      userId: i.string(),
+      name: i.string(),
     }),
-    "tags": i.entity({
-      "label": i.any(),
+    tags: i.entity({
+      label: i.string(),
     }),
   },
   {
-    "authorsPosts": {
-      "forward": {
-        "on": "authors",
-        "has": "many",
-        "label": "posts"
+    authorPosts: {
+      forward: {
+        on: "authors",
+        has: "many",
+        label: "posts",
       },
-      "reverse": {
-        "on": "posts",
-        "has": "one",
-        "label": "author"
-      }
+      reverse: {
+        on: "posts",
+        has: "one",
+        label: "author",
+      },
     },
-    "postsTags": {
-      "forward": {
-        "on": "posts",
-        "has": "many",
-        "label": "tags"
+    postsTags: {
+      forward: {
+        on: "posts",
+        has: "many",
+        label: "tags",
       },
-      "reverse": {
-        "on": "tags",
-        "has": "many",
-        "label": "posts"
-      }
-    }
-  }
+      reverse: {
+        on: "tags",
+        has: "many",
+        label: "posts",
+      },
+    },
+  },
 );
 
 export default graph;
