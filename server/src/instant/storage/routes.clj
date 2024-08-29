990337e0e34fff930964519e3a698354d30f653f
reichert621 <reichertjalex@gmail.com>
2024-08-29 15:16:30 -0700

    Refactor shared storage logic into utils

--- server/src/instant/storage/routes.clj
+++ server/src/instant/storage/routes.clj
@@ -5,13 +5,9 @@
             [instant.util.uuid :as uuid-util]
             [instant.util.string :as string-util]
             [instant.util.http :as http-util]
+            [instant.util.storage :as storage-util]
             [instant.model.app :as app-model]
-            [instant.model.app-user :as app-user-model]
-            [instant.model.rule :as rule-model]
-            [instant.storage.s3 :as s3-util]
-            [instant.storage.beta :as beta]
-            [instant.db.cel :as cel]
-            [instant.util.json :refer [->json <-json]]))
+            [instant.storage.s3 :as s3-util]))
 
 (defn buckets-get [_req]
   (let [buckets (s3-util/list-buckets)]
@@ -23,62 +19,26 @@
         objects (:object-summaries objects-resp)]
     (response/ok {:data (map #(select-keys % [:key :size :owner :etag]) objects)})))
 
-
 (comment
   (def b s3-util/default-bucket)
   (buckets-get {})
   (objects-get {:params {:bucket b}}))
 
-(comment
-  (def rules {:code
-              {"$files" {"bind" ["isLoggedIn" "auth.id != null"]
-                         "allow" {"view" "isLoggedIn"
-                                  "create" "data.path.contains('demo')"}}}})
-  (def program (rule-model/get-program! rules "$files" "create"))
-  (cel/eval-program! program {"auth" (cel/->cel-map {"id" "alex"})
-                              "data" (cel/->cel-map {"path" "demo/image.png"})}))
-
-(defn assert-storage-permission! [action {:keys [app-id refresh-token filepath]}]
-  (let [rules (rule-model/get-by-app-id {:app-id app-id})
-        current-user (when refresh-token
-                       (app-user-model/get-by-refresh-token {:app-id app-id
-                                                             :refresh-token refresh-token}))
-        program (rule-model/get-program! rules "$files" action)]
-    (ex/assert-permitted!
-     :has-storage-permission?
-     ["$files" action]
-     (if-not program
-       ;; deny access by default if no permissions are currently set
-       false
-       ;; otherwise, evaluate the permissions code
-       (cel/eval-program! program
-                          {"auth" (cel/->cel-map (<-json (->json current-user)))
-                           "data" (cel/->cel-map {"path" filepath})})))))
-
 (defn signed-download-url-get [req]
   (let [app-id-param (ex/get-param! req [:params :app_id] uuid-util/coerce)
         {app-id :id} (app-model/get-by-id! {:id app-id-param})
-        _ (beta/assert-storage-enabled! app-id)
         filename (ex/get-param! req [:params :filename] string-util/coerce-non-blank-str)
         refresh-token (http-util/req->bearer-token req)
-        expiration (+ (System/currentTimeMillis) (* 1000 60 60 24 7)) ;; 7 days
-        object-key (s3-util/->object-key app-id filename)]
-    (assert-storage-permission! "view" {:app-id app-id
-                                        :refresh-token refresh-token
-                                        :filepath filename})
-    (response/ok {:data (str (s3-util/signed-download-url object-key expiration))})))
+        data (storage-util/create-signed-download-url! app-id filename refresh-token)]
+    (response/ok {:data data})))
 
 (defn signed-upload-url-post [req]
   (let [app-id-param (ex/get-param! req [:body :app_id] uuid-util/coerce)
         {app-id :id} (app-model/get-by-id! {:id app-id-param})
-        _ (beta/assert-storage-enabled! app-id)
         filename (ex/get-param! req [:body :filename] string-util/coerce-non-blank-str)
         refresh-token (http-util/req->bearer-token req)
-        object-key (s3-util/->object-key app-id filename)]
-    (assert-storage-permission! "create" {:app-id app-id
-                                          :refresh-token refresh-token
-                                          :filepath filename})
-    (response/ok {:data (str (s3-util/signed-upload-url object-key))})))
+        data (storage-util/create-signed-upload-url! app-id filename refresh-token)]
+    (response/ok {:data data})))
 
 (defroutes routes
   (POST "/storage/signed-upload-url" [] signed-upload-url-post)
---
+++ server/src/instant/util/storage.clj
@@ -0,0 +1,88 @@
+(ns instant.util.storage
+  (:require [clojure.string :as string]
+            [instant.util.exception :as ex]
+            [instant.model.app-user :as app-user-model]
+            [instant.model.rule :as rule-model]
+            [instant.db.cel :as cel]
+            [instant.util.json :refer [->json <-json]]
+            [instant.storage.s3 :as s3-util]
+            [instant.storage.beta :as storage-beta]))
+
+(comment
+  (def rules {:code
+              {"$files" {"bind" ["isLoggedIn" "auth.id != null"]
+                         "allow" {"view" "isLoggedIn"
+                                  "create" "data.path.contains('demo')"}}}})
+  (def program (rule-model/get-program! rules "$files" "create"))
+  (cel/eval-program! program {"auth" (cel/->cel-map {"id" "alex"})
+                              "data" (cel/->cel-map {"path" "demo/image.png"})}))
+
+
+(defn assert-storage-permission! [action {:keys [app-id refresh-token filepath]}]
+  (let [rules (rule-model/get-by-app-id {:app-id app-id})
+        current-user (when refresh-token
+                       (app-user-model/get-by-refresh-token {:app-id app-id
+                                                             :refresh-token refresh-token}))
+        program (rule-model/get-program! rules "$files" action)]
+    (ex/assert-permitted!
+     :has-storage-permission?
+     ["$files" action]
+     (if-not program
+       ;; deny access by default if no permissions are currently set
+       false
+       ;; otherwise, evaluate the permissions code
+       (cel/eval-program! program
+                          {"auth" (cel/->cel-map (<-json (->json current-user)))
+                           "data" (cel/->cel-map {"path" filepath})})))))
+
+(defn create-signed-download-url!
+  ([app-id filename refresh-token]
+   (assert-storage-permission! "read" {:app-id app-id
+                                       :refresh-token refresh-token
+                                       :filepath filename})
+   (create-signed-download-url! app-id filename))
+  ([app-id filename]
+   (let [expiration (+ (System/currentTimeMillis) (* 1000 60 60 24 7)) ;; 7 days
+         object-key (s3-util/->object-key app-id filename)]
+     (storage-beta/assert-storage-enabled! app-id)
+     (str (s3-util/signed-download-url object-key expiration)))))
+
+(defn create-signed-upload-url!
+  ([app-id filename refresh-token]
+   (assert-storage-permission! "create" {:app-id app-id
+                                         :refresh-token refresh-token
+                                         :filepath filename})
+   (create-signed-upload-url! app-id filename))
+  ([app-id filename]
+   (let [object-key (s3-util/->object-key app-id filename)]
+     (storage-beta/assert-storage-enabled! app-id)
+     (str (s3-util/signed-upload-url object-key)))))
+
+(defn format-object [{:keys [key size owner etag last-modified]}]
+  {:key key
+   :size size
+   :owner owner
+   :etag etag
+   :last_modified (.getMillis last-modified)})
+
+;; Retrieves all files that have been uploaded via Storage APIs
+(defn list-files! [app-id subdirectory]
+  (let [_ (storage-beta/assert-storage-enabled! app-id)
+        prefix (if (string/blank? subdirectory)
+                 (app-id)
+                 (str app-id "/" subdirectory))
+        objects-resp (s3-util/list-app-objects prefix)
+        objects (:object-summaries objects-resp)]
+    (map format-object objects)))
+
+;; Deletes a single file by name/path (e.g. "demo.png", "profiles/me.jpg")
+(defn delete-file! [app-id filename]
+  (let [_ (storage-beta/assert-storage-enabled! app-id)
+        key (s3-util/->object-key app-id filename)]
+    (s3-util/delete-object key)))
+
+;; Deletes a multiple files by name/path (e.g. "demo.png", "profiles/me.jpg")
+(defn bulk-delete-files! [app-id filenames]
+  (let [_ (storage-beta/assert-storage-enabled! app-id)
+        keys (mapv (fn [filename] (s3-util/->object-key app-id filename)) filenames)]
+    (s3-util/delete-objects keys)))
