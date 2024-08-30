68074b3211ccb8a7c76f072951620fd0bd9a71de
Daniel Woelfel <dwwoelfel@gmail.com>
2024-08-29 22:49:06 -0700

    cleanup

--- client/www/components/dash/AppAuth.tsx
+++ client/www/components/dash/AppAuth.tsx
@@ -796,11 +796,15 @@ function AddClerkProviderForm({
   );
 }
 
+// Base64 decode, switching to url-safe decode if we hit an error
+// Can't be sure which method Clerk uses because you can't generate
+// `+` or `/` with characters that go in a normal host. Urls with
+// chinese characters exist, they might encode to `+` or `/`, and
+// Clerk might support them, so we'll be safe and do both.
 function base64Decode(s: string) {
   try {
     return Buffer.from(s, 'base64').toString('utf-8');
   } catch (e) {
-    console.error('e', e);
     return Buffer.from(s, 'base64url').toString('utf-8');
   }
 }
