2de9dba53f865b760bf77a6261079a9152851798
Marky @ Instant <marky@instantdb.com>
2024-09-03 11:16:57 -0700

    examples and fixes

--- client/packages/core/vite.config.ts
+++ client/packages/core/vite.config.ts
@@ -8,8 +8,11 @@ export default defineConfig({
       formats: ["umd", "es"],
       // this is the file that exports our components
       entry: resolve(__dirname, "src", "index.ts"),
-      name: "instant-core",
+      name: "instant",
       fileName: "index",
     },
+  },
+  define: {
+    "process.env": {},
   },
 });
--- client/packages/react/vite.config.ts
+++ client/packages/react/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       formats: ["umd", "es"],
       // this is the file that exports our components
       entry: resolve(__dirname, "src", "index.ts"),
-      name: "instant-react",
+      name: "instantReact",
       fileName: "index",
     },
     rollupOptions: {
@@ -26,5 +26,8 @@ export default defineConfig({
         },
       },
     },
+  },
+  define: {
+    "process.env": {},
   },
 });
