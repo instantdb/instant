2b3bbb015f7e3a8f6bbf6e23f68603040e6fca59
reichert621 <reichertjalex@gmail.com>
2024-09-04 11:09:52 -0400

    Add migration scripts for personal access tokens

---
+++ server/resources/migrations/30_personal_access_tokens_up.sql
@@ -0,0 +1,8 @@
+create table instant_personal_access_tokens (
+  id uuid primary key,
+  created_at timestamp not null default now(),
+  user_id uuid references instant_users(id) on delete cascade
+);
+
+create index instant_personal_access_tokens_user_id_index
+  on instant_personal_access_tokens (user_id);
