c9b99e4eeffaff5961e2db9c2d76ca93f5f4472f
reichert621 <reichertjalex@gmail.com>
2024-09-04 13:38:04 -0400

    Improve naming

--- server/src/instant/model/app.clj
+++ server/src/instant/model/app.clj
@@ -36,8 +36,8 @@
 (defn get-by-id! [params]
   (ex/assert-record! (get-by-id params) :app {:args [params]}))
 
-(defn list-account-apps
-  ([user-id] (list-account-apps aurora/conn-pool user-id))
+(defn list-by-creator-id
+  ([user-id] (list-by-creator-id aurora/conn-pool user-id))
   ([conn user-id]
    (sql/select conn
                ["SELECT a.*
@@ -46,11 +46,11 @@
                 user-id])))
 
 (comment
-  (def u "6412d553-2749-4f52-898a-0b3ec42ffd28")
-  (list-account-apps u))
+  (def user-id "6412d553-2749-4f52-898a-0b3ec42ffd28")
+  (list-by-creator-id user-id))
 
-(defn get-account-app-by-id
-  ([params] (get-account-app-by-id aurora/conn-pool params))
+(defn get-creator-app-by-id
+  ([params] (get-creator-app-by-id aurora/conn-pool params))
   ([conn {:keys [user-id app-id]}]
    (sql/select-one conn
                    ["SELECT a.*
@@ -60,13 +60,13 @@
                       a.creator_id = ?::uuid"
                     app-id user-id])))
 
-(defn get-account-app-by-id! [params]
-  (ex/assert-record! (get-account-app-by-id params) :app {:args [params]}))
+(defn get-creator-app-by-id! [params]
+  (ex/assert-record! (get-creator-app-by-id params) :app {:args [params]}))
 
 (comment
-  (def u "6412d553-2749-4f52-898a-0b3ec42ffd28")
+  (def user-id "6412d553-2749-4f52-898a-0b3ec42ffd28")
   (def app-id "68b75bac-3ff7-4efe-9596-97ac0d03ab65")
-  (get-account-app-by-id {:user-id u :app-id app-id}))
+  (get-creator-app-by-id {:user-id user-id :app-id app-id}))
 
 (defn get-app-ids-created-before
   ([params] (get-app-ids-created-before aurora/conn-pool params))
