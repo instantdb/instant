eb2b178e9f435576ba0b0b1b37f880ceca614126
marky <1624703+markyfyi@users.noreply.github.com>
2024-09-06 13:55:37 -0700

    String Init: Use schema instead of attrs when resolving cardinality (#157)

---
+++ client/packages/core/src/utils/linkIndex.ts
@@ -0,0 +1,33 @@
+import { InstantGraph, LinkDef, LinksDef } from "../schema";
+
+export type LinkIndex = Record<
+  string,
+  Record<
+    string,
+    {
+      isForward: boolean;
+      isSingular: boolean;
+      link: LinkDef<any, any, any, any, any, any, any>;
+    }
+  >
+>;
+
+export function createLinkIndex(schema: InstantGraph<any, LinksDef<{}>>) {
+  return Object.values(schema.links).reduce((linkIndex, link) => {
+    linkIndex[link.forward.on] ??= {};
+    linkIndex[link.forward.on][link.forward.label] = {
+      isForward: true,
+      isSingular: link.forward.has === "one",
+      link,
+    };
+
+    linkIndex[link.reverse.on] ??= {};
+    linkIndex[link.reverse.on][link.reverse.label] = {
+      isForward: false,
+      isSingular: link.reverse.has === "one",
+      link,
+    };
+
+    return linkIndex;
+  }, {} as LinkIndex);
+}
