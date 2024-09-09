b771c0d25510aa91a03ca4ed8a41628539b0770a
stopachka <stepan.p@gmail.com>
2024-09-09 10:35:04 -0700

    smfix

--- client/sandbox/react-nextjs/app/play/route-hydration/[[...slug]]/page.tsx
+++ client/sandbox/react-nextjs/app/play/route-hydration/[[...slug]]/page.tsx
@@ -132,7 +132,7 @@ function Main({ appIsLoading }: { appIsLoading: boolean | null }) {
   }, []);
   return (
     <div className="p-4">
-      <div>
+      <div className="space-x-2 text-blue-500">
         <Link href="/play/route-hydration/1">page 1</Link>
         <Link href="/play/route-hydration/2">page 2</Link>
       </div>
@@ -148,14 +148,12 @@ function Main({ appIsLoading }: { appIsLoading: boolean | null }) {
           <li>
             <strong>The first time you load this page</strong>
             <ul>
-              <li>App:auth:loading will be `true`</li>
+              <li>App:auth:loading will be `true`</li> (because IDB _has not_ loaded yet)
               <li>
-                Main:auth:loading will be `false` (because IDB has already
-                loaded)
+                Main:auth:loading will be `false` (because IDB has already loaded)
               </li>
               <li>
-                Main:query:loading will be `false` (because IDB has already
-                loaded)
+                Main:query:loading will be `false` (because IDB has already loaded)
               </li>
             </ul>
           </li>
