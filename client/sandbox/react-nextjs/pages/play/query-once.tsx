98393d2cdf7a3da93046d12b655db9973583f12a
Joe Averbukh <joeaverbukh@gmail.com>
2024-09-27 12:07:33 -0400

    Add fuller queryOnce example

--- client/sandbox/react-nextjs/pages/play/query-once.tsx
+++ client/sandbox/react-nextjs/pages/play/query-once.tsx
@@ -1,27 +1,101 @@
-import { init } from "@instantdb/react";
-import { useEffect } from "react";
+/*
+ * Query Once example
+ * The goal here is to show how we can use queryOnce to fetch data
+ * without subscribing to updates but still get data from the server
+ * to validate if the item already exists.
+ * */
+
+import { init, id, tx } from "@instantdb/react";
+import Head from "next/head";
+import { useEffect, FormEvent } from "react";
 import config from "../../config";
 
 const db = init<{
-  habits: {
-    name: string;
+  onceTest: {
+    text: string;
   };
 }>(config);
 
-async function queryOnceDemo() {
+async function queryOnceDemo(newItem: string) {
+  console.log("newItem", newItem);
   const res = await db.queryOnce({
-    habits: {
-      $: { limit: 1 },
-    },
+    onceTest: { $: { where: { 'text': newItem } } },
   });
-
-  console.log(res.data.habits);
+  return res.data.onceTest.length > 0;
 }
 
-export default function () {
-  useEffect(() => {
-    queryOnceDemo();
-  }, []);
-
-  return null;
+function addOnce(text: string) {
+  db.transact(
+    tx.onceTest[id()].update({ text, }),
+  );
 }
+
+function deleteAll(items: any) {
+  db.transact(items.map((item: any) => tx.onceTest[item.id].delete()));
+}
+
+interface FormProps {
+  addOnce: (value: string) => void;
+}
+const TodoForm: React.FC<FormProps> = ({ addOnce }) => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const input = e.currentTarget.elements.namedItem('todoInput') as HTMLInputElement;
+    if (input && input.value.trim()) {
+      if (await queryOnceDemo(input.value)) {
+        alert('Item already exists');
+      } else {
+        addOnce(input.value);
+        input.value = '';
+      }
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        name="todoInput"
+        autoFocus
+        placeholder="What needs to be done?"
+        type="text"
+      />
+    </form>
+  );
+};
+
+function Main() {
+  const { isLoading, error, data } = db.useQuery({
+    onceTest: {},
+  });
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+  return (
+    <div className="p-2">
+      <div className="flex space-x-2 py-2">
+        <div>Query Once test</div>
+        <button className="border px-4 border-black" onClick={() => deleteAll(data.onceTest)}>Delete All</button>
+      </div>
+      <TodoForm addOnce={addOnce} />
+      {data.onceTest.map((todo: any) => (
+        <div key={todo.id}>{todo.text}</div>
+      ))}
+    </div>
+  )
+}
+
+function Page() {
+  return (
+    <div>
+      <Head>
+        <title>Instant Example App: Query Once</title>
+        <meta
+          name="description"
+          content="Relational Database, on the client."
+        />
+      </Head>
+      <Main />
+    </div>
+  );
+}
+
+export default Page;
