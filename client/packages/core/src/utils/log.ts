879f50a3344d5a03a298d1a87a4f0baffeeae480
stopachka <stepan.p@gmail.com>
2024-10-14 11:39:15 -0700

    make log work by default for devBackend

--- client/packages/core/src/utils/log.ts
+++ client/packages/core/src/utils/log.ts
@@ -3,7 +3,9 @@ if (
   typeof window !== "undefined" &&
   typeof window.localStorage !== "undefined"
 ) {
-  isEnabled = !!window.localStorage.getItem("__instantLogging");
+  isEnabled =
+    !!window.localStorage.getItem("devBackend") ||
+    !!window.localStorage.getItem("__instantLogging");
 }
 
 const log = {
