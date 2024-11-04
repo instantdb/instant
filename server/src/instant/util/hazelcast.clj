a89f4efd658e07bfc9c881b7242080d0f89229cb
Daniel Woelfel <dwwoelfel@gmail.com>
2024-11-04 09:24:04 -0800

    remove debug code

--- server/src/instant/util/hazelcast.clj
+++ server/src/instant/util/hazelcast.clj
@@ -197,10 +197,8 @@
     ;; Must be unique within the project
     (getTypeId [_] 5)
     (write ^bytes [_ obj]
-      (tool/def-locals)
       (nippy/fast-freeze obj))
     (read [_ ^bytes in]
-      (tool/def-locals)
       (nippy/fast-thaw in))
     (destroy [_])))
 
