dfb12882fb8bb526c20f552ce8ba233e2ff632f0
Stepan Parunashvili <stepan.p@gmail.com>
2024-11-06 10:49:01 -0800

    [clean] remove stray sandbox clients: perf_1 and tx_order (#435)

--- client/sandbox/perf_1/src/db.ts
+++
@@ -1,9 +0,0 @@
-import { init } from '@instantdb/react';
-import { Schema } from '@/Schema';
-
-export const db = init<Schema>({
-  appId: (import.meta as any).env.VITE_INSTANT_APP_ID,
-  apiURI: 'http://localhost:8888',
-  websocketURI: 'ws://localhost:8888/runtime/session',
-  devtool: false,
-});
