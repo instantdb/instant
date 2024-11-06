037e02ea0483cc83c205e88e62eabb82fb769d77
stopachka <stepan.p@gmail.com>
2024-11-06 11:01:11 -0800

    roar

---
+++ client/sandbox/strong-init-vite/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>Hello world</div>
+  )
+}
+
+export default App
