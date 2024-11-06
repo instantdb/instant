15b604960ce7605c3364976c89aec24348ffcdd0
Stepan Parunashvili <stepan.p@gmail.com>
2024-11-06 12:02:08 -0800

    [strong init] create a sandbox (#436)

---
+++ client/sandbox/strong-init-vite/instant.schema.ts
@@ -0,0 +1,28 @@
+import { i } from "@instantdb/react";
+
+const graph = i.graph(
+  {
+    messages: i.entity({
+      content: i.string(),
+    }),
+    $users: i.entity({
+      email: i.string().unique().indexed(),
+    }),
+  },
+  {
+    messageCreator: {
+      forward: {
+        on: "messages",
+        has: "one",
+        label: "creator",
+      },
+      reverse: {
+        on: "$users",
+        has: "many",
+        label: "createdMessages",
+      },
+    },
+  },
+);
+
+export default graph;
