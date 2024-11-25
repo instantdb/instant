3a18ff3a67e8af099def26a2e0086ac1c905f98b
stopachka <stepan.p@gmail.com>
2024-11-25 13:59:05 -0800

    roar

--- client/packages/cli/index.js
+++ client/packages/cli/index.js
@@ -1200,26 +1200,25 @@ async function pushPerms(appId) {
 async function waitForAuthToken({ secret }) {
   for (let i = 1; i <= 120; i++) {
     await sleep(1000);
-
-    try {
-      const authCheckRes = await fetchJson({
-        method: "POST",
-        debugName: "Auth check",
-        errorMessage: "Failed to check auth status.",
-        path: "/dash/cli/auth/check",
-        body: { secret },
-        noAuth: true,
-        noLogError: true,
-      });
-
-      if (authCheckRes.ok) {
-        return authCheckRes.data;
-      }
-    } catch (error) {
-      throw error;
+    const authCheckRes = await fetchJson({
+      method: "POST",
+      debugName: "Auth check",
+      errorMessage: "Failed to check auth status.",
+      path: "/dash/cli/auth/check",
+      body: { secret },
+      noAuth: true,
+      noLogError: true,
+    });
+    if (authCheckRes.ok) {
+      return authCheckRes.data;
     }
+    if (authCheckRes.data?.hint.errors?.[0]?.issue === "waiting-for-user") {
+      continue;
+    }
+    error('Failed to authenticate ');
+    prettyPrintJSONErr(authCheckRes.data);
+    return;
   }
-
   error("Timed out waiting for authentication");
   return null;
 }
@@ -1287,17 +1286,7 @@ async function fetchJson({
     if (!res.ok) {
       if (withErrorLogging) {
         error(errorMessage);
-        if (data?.message) {
-          error(data.message);
-        }
-        if (Array.isArray(data?.hint?.errors)) {
-          for (const err of data.hint.errors) {
-            error(`${err.in ? err.in.join("->") + ": " : ""}${err.message}`);
-          }
-        }
-        if (!data) {
-          error("Failed to parse error response");
-        }
+        prettyPrintJSONErr(data);
       }
       return { ok: false, data };
     }
@@ -1318,6 +1307,20 @@ async function fetchJson({
       }
     }
     return { ok: false, data: null };
+  }
+}
+
+function prettyPrintJSONErr(data) {
+  if (data?.message) {
+    error(data.message);
+  }
+  if (Array.isArray(data?.hint?.errors)) {
+    for (const err of data.hint.errors) {
+      error(`${err.in ? err.in.join("->") + ": " : ""}${err.message}`);
+    }
+  }
+  if (!data) {
+    error("Failed to parse error response");
   }
 }
 
