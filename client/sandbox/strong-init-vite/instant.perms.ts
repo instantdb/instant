c0bbf38e680dfddb318a1e8e1ace4aca61572dfc
stopachka <stepan.p@gmail.com>
2024-11-26 15:45:06 -0800

    roar

--- client/sandbox/strong-init-vite/instant.perms.ts
+++ client/sandbox/strong-init-vite/instant.perms.ts
@@ -1,9 +1,22 @@
+type InstantRules = {
+  [EntityName: string]: {
+    allow: {
+      view?: string;
+      create?: string;
+      update?: string;
+      delete?: string;
+    };
+    bind?: string[];
+  };
+};
+
 const rules = {
   attrs: {
     allow: {
       create: "false",
     },
+    bind: ["admin", "foo", "bar", "biz"],
   },
-};
+} satisfies InstantRules;
 
 export default rules;
