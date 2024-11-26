9b25ac78ef6a952e5509b97a263dcd6be952ad1c
Daniel Woelfel <dwwoelfel@gmail.com>
2024-11-25 18:39:24 -0800

    gzip honeycomb traces (#542)

--- server/src/instant/util/tracer.clj
+++ server/src/instant/util/tracer.clj
@@ -6,13 +6,14 @@
    [instant.config :as config]
    [steffan-westcott.clj-otel.api.attributes :as attr])
   (:import
-   (io.honeycomb.opentelemetry OpenTelemetryConfiguration)
    (io.opentelemetry.sdk OpenTelemetrySdk)
+   (io.opentelemetry.api.common Attributes AttributeKey)
    (io.opentelemetry.api.trace Span StatusCode)
    (io.opentelemetry.context Context)
-   (io.opentelemetry.sdk.trace SdkTracerProvider)
-   (io.opentelemetry.semconv.trace.attributes SemanticAttributes)
-   (io.opentelemetry.sdk.trace.export SimpleSpanProcessor))
+   (io.opentelemetry.exporter.otlp.trace OtlpGrpcSpanExporter)
+   (io.opentelemetry.sdk.resources Resource)
+   (io.opentelemetry.sdk.trace SdkTracerProvider SdkTracer)
+   (io.opentelemetry.sdk.trace.export BatchSpanProcessor SimpleSpanProcessor))
   (:gen-class))
 
 (def ^:dynamic *span* nil)
@@ -22,7 +23,7 @@
 (def ^:dynamic *silence-exceptions?* nil)
 
 (defonce tracer (atom nil))
-(defn get-tracer []
+(defn get-tracer ^SdkTracer []
   (if-let [t @tracer]
     t
     (throw (Exception. "Call to trace before initialization."))))
@@ -30,6 +31,7 @@
 (defn make-log-only-sdk
   "Creates an opentelemetry sdk that logs to the console for use in tests
   and for Open Source when no Honeycomb API key is available."
+  ^OpenTelemetrySdk
   []
   (let [trace-provider-builder (SdkTracerProvider/builder)
         sdk-builder (OpenTelemetrySdk/builder)
@@ -39,16 +41,31 @@
         (.setTracerProvider (.build trace-provider-builder))
         (.build))))
 
-(defn make-honeycomb-sdk [honeycomb-api-key]
-  (let [builder (OpenTelemetryConfiguration/builder)
-        log-processor (SimpleSpanProcessor/create (logging-exporter/create))]
-    (-> builder
-        (.setApiKey honeycomb-api-key)
-        (.setEndpoint (config/get-honeycomb-endpoint))
-        (.setDataset "metrics")
-        (.setServiceName "instant-server")
-        (.addSpanProcessor log-processor)
-        (.buildAndRegisterGlobal))))
+(defn make-honeycomb-sdk
+  ^OpenTelemetrySdk
+  [honeycomb-api-key]
+  (let [trace-provider-builder (SdkTracerProvider/builder)
+        sdk-builder (OpenTelemetrySdk/builder)
+        log-processor (SimpleSpanProcessor/create (logging-exporter/create))
+        otlp-builder (OtlpGrpcSpanExporter/builder)
+        resource (.merge (Resource/getDefault)
+                         (Resource/create (Attributes/of (AttributeKey/stringKey "service.name")
+                                                         "instant-server")))]
+
+    (.setResource trace-provider-builder resource)
+    (.setCompression otlp-builder "gzip")
+
+    (.setEndpoint otlp-builder (config/get-honeycomb-endpoint))
+    (.addHeader otlp-builder "x-honeycomb-team" honeycomb-api-key)
+
+    (.addSpanProcessor trace-provider-builder
+                       (.build (BatchSpanProcessor/builder (.build otlp-builder))))
+
+    (.addSpanProcessor trace-provider-builder log-processor)
+
+    (-> sdk-builder
+        (.setTracerProvider (.build trace-provider-builder))
+        (.build))))
 
 (defn init []
   (let [sdk (if-let [honeycomb-api-key (config/get-honeycomb-api-key)]
@@ -71,13 +88,13 @@
         default-attributes (cond-> @last-calculated-metrics
                              true (assoc "host.name" (config/get-hostname)
                                          "process-id" @config/process-id)
-                             thread (assoc SemanticAttributes/THREAD_NAME
+                             thread (assoc "thread.name"
                                            (.getName thread)
-                                           SemanticAttributes/THREAD_ID
+                                           "thread.id"
                                            (.getId thread))
-                             code-ns     (assoc SemanticAttributes/CODE_NAMESPACE code-ns)
-                             code-line   (assoc SemanticAttributes/CODE_LINENO code-line)
-                             code-file   (assoc SemanticAttributes/CODE_FILEPATH code-file))
+                             code-ns     (assoc "code.namespace" code-ns)
+                             code-line   (assoc "code.lineno" code-line)
+                             code-file   (assoc "code.filepath" code-file))
         attributes'  (merge default-attributes attributes)]
     (-> (get-tracer)
         (.spanBuilder (name span-name))
@@ -98,7 +115,7 @@
    {:keys [exception escaping? attributes]
     :or   {attributes {}}}]
   (let [attrs (cond-> attributes
-                escaping? (assoc SemanticAttributes/EXCEPTION_ESCAPED (boolean escaping?)))]
+                escaping? (assoc "exception.escaped" (boolean escaping?)))]
     (.recordException span exception (attr/->attributes attrs))))
 
 (defn add-data!
@@ -198,7 +215,7 @@
   ([]
    (when *span*
      (span-uri *span*)))
-  ([span]
+  ([^Span span]
    (let [ctx (.getSpanContext span)
          trace-id (.getTraceId ctx)
          span-id (.getSpanId ctx)]
