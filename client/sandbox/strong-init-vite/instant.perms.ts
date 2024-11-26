2b7baf497a981c5684b9ea5b2831de3eba9a8d93
stopachka <stepan.p@gmail.com>
2024-11-26 15:50:22 -0800

    roar

--- client/sandbox/strong-init-vite/instant.perms.ts
+++ client/sandbox/strong-init-vite/instant.perms.ts
@@ -1,21 +1,33 @@
-type InstantRules = {
-  [EntityName: string]: {
-    allow: {
-      view?: string;
-      create?: string;
-      update?: string;
-      delete?: string;
-    };
-    bind?: string[];
-  };
-};
+// Docs: https://www.instantdb.com/docs/permissions
+
+import { type InstantRules } from "@instantdb/react";
 
 const rules = {
   attrs: {
     allow: {
       create: "false",
     },
-    bind: ["admin", "foo", "bar", "biz"],
+  },
+  posts: {
+    bind: [
+      "isAdmin",
+      "auth.email == 'stepan.p@gmail.com'",
+      "isOwner",
+      "auth.uid == data.author",
+    ],
+    allow: {
+      create: "isAdmin || isOwner",
+      delete: "isAdmin || isOwner",
+      update: "isAdmin || isOwner",
+    },
+  },
+  postBodies: {
+    bind: ["isAdmin", "auth.email == 'stepan.p@gmail.com'"],
+    allow: {
+      create: "isAdmin",
+      delete: "isAdmin",
+      update: "isAdmin",
+    },
   },
 } satisfies InstantRules;
 
