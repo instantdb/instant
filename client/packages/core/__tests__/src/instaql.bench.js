a3725ccff2158307de0fb35e4605ed244f06bd6f
Daniel Woelfel <dwwoelfel@gmail.com>
2024-12-12 11:36:10 -0800

    run a benchmark

---
+++ client/packages/core/__tests__/src/instaql.bench.js
@@ -0,0 +1,29 @@
+import { bench } from "vitest";
+
+import zenecaAttrs from "./data/zeneca/attrs.json";
+import zenecaTriples from "./data/zeneca/triples.json";
+import { createStore } from "../../src/store";
+import query from "../../src/instaql";
+
+const zenecaIdToAttr = zenecaAttrs.reduce((res, x) => {
+  res[x.id] = x;
+  return res;
+}, {});
+
+const store = createStore(zenecaIdToAttr, zenecaTriples);
+
+bench("big query", () => {
+  query(
+    { store },
+    {
+      users: {
+        bookshelves: {
+          books: {},
+          users: {
+            bookshelves: {},
+          },
+        },
+      },
+    },
+  );
+});
