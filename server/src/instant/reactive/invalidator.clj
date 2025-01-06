6172c4099129dfac73f112a7c6e206b243059ad9
Daniel Woelfel <dwwoelfel@gmail.com>
2025-01-06 10:19:55 -0800

    Send one span for all `invalidator/send-refresh` (#674)

--- server/src/instant/reactive/invalidator.clj
+++ server/src/instant/reactive/invalidator.clj
@@ -303,9 +303,8 @@
             (try
               (let [sockets (invalidate! store-conn wal-record)]
                 (tracer/add-data! {:attributes {:num-sockets (count sockets)}})
-                (doseq [{:keys [id]} sockets]
-                  (tracer/with-span! {:name "invalidator/send-refresh"
-                                      :attributes {:session-id id}}
+                (tracer/with-span! {:name "invalidator/send-refreshes"}
+                  (doseq [{:keys [id]} sockets]
                     (receive-queue/enqueue->receive-q {:op :refresh
                                                        :session-id id}))))
               (catch Throwable t
@@ -319,9 +318,8 @@
     (try
       (let [sockets (invalidate-byop! table-info app-id store-conn record)]
         (tracer/add-data! {:attributes {:num-sockets (count sockets)}})
-        (doseq [{:keys [id]} sockets]
-          (tracer/with-span! {:name "invalidator/send-refresh"
-                              :session-id id}
+        (tracer/with-span! {:name "invalidator/send-refreshes"}
+          (doseq [{:keys [id]} sockets]
             (receive-queue/enqueue->receive-q {:op :refresh
                                                :session-id id}))))
       (catch Throwable t
