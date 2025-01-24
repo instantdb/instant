98b5387cab070b5a09dc1d890cc64e28ca27683e
Joe Averbukh <joeaverbukh@gmail.com>
2025-01-23 16:15:03 -0800

    Add storage coordinator

---
+++ server/src/instant/storage/coordinator.clj
@@ -0,0 +1,111 @@
+(ns instant.storage.coordinator
+  (:require [instant.storage.s3 :as instant-s3]
+            [instant.model.app-file :as app-file-model]
+            [instant.model.rule :as rule-model]
+            [instant.storage.beta :as storage-beta]
+            [instant.util.exception :as ex]
+            [instant.db.cel :as cel]
+            [instant.model.app-upload-url :as app-upload-url-model]
+            [instant.config :as config]))
+
+(defn assert-storage-permission! [action {:keys [app-id
+                                                 path
+                                                 current-user
+                                                 rules-override]
+                                          :as ctx}]
+  (let [rules (if rules-override
+                rules-override
+                (rule-model/get-by-app-id {:app-id app-id}))
+        program (rule-model/get-program! rules "$files" action)]
+    (ex/assert-permitted!
+     :has-storage-permission?
+     ["$files" action]
+     (if-not program
+       ;; deny access by default if no permissions are currently set
+       false
+       ;; otherwise, evaluate the permissions code
+       (cel/eval-program! program
+                          {"auth" (cel/->cel-map {:type :auth
+                                                  :ctx ctx
+                                                  :etype "$users"}
+                                                 current-user)
+                           "data" (cel/->cel-map {:type :data
+                                                  :ctx ctx
+                                                  :etype "$files"}
+                                                 {"path" path})})))))
+
+(defn upload-file!
+  "Uploads a file to S3 and tracks it in Instant."
+  [{:keys [app-id path skip-perms-check? current-user] :as ctx} file]
+  (storage-beta/assert-storage-enabled! app-id)
+  (when (not skip-perms-check?)
+    (assert-storage-permission! "create" {:app-id app-id
+                                          :path path
+                                          :current-user current-user}))
+  (instant-s3/upload-file-to-s3 ctx file)
+  (let [metadata (instant-s3/get-object-metadata app-id path)]
+    (app-file-model/create! {:app-id app-id :path path :metadata metadata})))
+
+(defn delete-files!
+  "Deletes multiple files from both Instant and S3.
+  Returns a set of deleted entity IDs."
+  [{:keys [app-id paths]}]
+  (storage-beta/assert-storage-enabled! app-id)
+  (when (seq paths)
+    (let [tx-res (app-file-model/delete-by-paths! {:app-id app-id :paths paths})
+          _ (instant-s3/bulk-delete-files! app-id paths)]
+      (-> tx-res
+          :results
+          :delete-entity
+          (->> (map :triples/entity_id)
+               set)))))
+
+(defn delete-file!
+  "Deletes a file from both Instant and S3. Returns a deleted entity ID."
+  [{:keys [app-id path current-user]}]
+  (assert-storage-permission! "delete" {:app-id app-id
+                                        :path path
+                                        :current-user current-user})
+  (first (delete-files! {:app-id app-id :paths [path]})))
+
+;; Logic for legacy S3 upload/download URLs
+;; -------------------------
+
+(defn create-upload-url
+  "Returns a temporary url for uploading a file to Instant"
+  [{:keys [app-id path skip-perms-check? current-user]}]
+  (storage-beta/assert-storage-enabled! app-id)
+  (when (not skip-perms-check?)
+    (assert-storage-permission! "create" {:app-id app-id
+                                          :path path
+                                          :current-user current-user}))
+  (let [{upload-id :id} (app-upload-url-model/create! {:app-id app-id :path path})]
+    (str config/server-origin "/storage/" upload-id "/consume-upload-url")))
+
+(defn consume-upload-url!
+  "Consume an Instant upload url and if it's valid kicks off our upload process"
+  [{:keys [upload-id content-type]} file]
+  (let [{app-id :app_id path :path expired-at :expired_at}
+        (app-upload-url-model/consume! {:upload-id upload-id})]
+
+    (when (or (not expired-at)
+              (.isBefore (.toInstant expired-at) (java.time.Instant/now)))
+      (throw (ex/throw-validation-err!
+              :app-upload-url
+              upload-id
+              "The upload URL is expired or invalid.")))
+    (upload-file!
+     {:app-id app-id
+      :path path
+      :content-type content-type
+      :skip-perms-check? true} file)))
+
+(defn create-download-url
+  "Returns a temporary url for downloading a file from Instant"
+  [{:keys [app-id path skip-perms-check? current-user]}]
+  (storage-beta/assert-storage-enabled! app-id)
+  (when (not skip-perms-check?)
+    (assert-storage-permission! "view" {:app-id app-id
+                                        :path path
+                                        :current-user current-user}))
+  (instant-s3/create-signed-download-url! app-id path))
