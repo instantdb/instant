b36e9edc85d898329ad34ef8810fd0a2c318d60b
Joe Averbukh <joeaverbukh@gmail.com>
2025-01-23 16:09:58 -0800

    Reorganize and update Instant <> S3 integration

--- server/src/instant/storage/s3.clj
+++ server/src/instant/storage/s3.clj
@@ -1,114 +1,94 @@
 (ns instant.storage.s3
-  (:require [clojure.java.io :as io]
-            [clj-http.client :as clj-http]
-            [amazonica.aws.s3 :as s3]
-            [instant.config :as config]))
+  (:require [clojure.string :as string]
+            [instant.util.s3 :as s3-util]))
 
-(def default-bucket "instant-storage")
+;; S3 path manipulation
+;; ----------------------
 
-(defn list-buckets []
-  (s3/list-buckets))
+(defn ->object-key
+  "Combine app-id and filename to provide a full S3 object key"
+  [app-id filename]
+  (if (string/starts-with? filename "/")
+    (str app-id filename)
+    (str app-id "/" filename)))
 
-(defn list-objects
-  ([] (list-objects default-bucket))
-  ([bucket-name]
-   (s3/list-objects bucket-name)))
+(defn object-key->app-id
+  "Extract app-id from our S3 object keys"
+  [object-key]
+  (first (string/split object-key #"/")))
 
-(defn list-objects-v2
-  ([opts] (list-objects-v2 default-bucket opts))
-  ([bucket-name opts]
-   (s3/list-objects-v2 (merge {:bucket-name bucket-name} opts))))
+(defn object-key->path
+  "Extract path from our S3 object keys"
+  [object-key]
+  (string/join "/" (rest (string/split object-key #"/"))))
 
-(defn get-object
-  ([object-key] (get-object default-bucket object-key))
-  ([bucket-name object-key]
-   (s3/get-object {:bucket-name bucket-name :key object-key})))
+;; Instant <> S3 integration
+;; ----------------------
 
-(defn delete-object
-  ([object-key] (delete-object default-bucket object-key))
-  ([bucket-name object-key]
-   (s3/delete-object {:bucket-name bucket-name
-                      :key object-key})))
+(defn upload-file-to-s3 [{:keys [app-id path] :as ctx} file]
+  (let [ctx* (assoc ctx :object-key (->object-key app-id path))]
+    (cond
+      (instance? java.io.InputStream file)
+      (s3-util/upload-stream-to-s3 ctx* file)
 
-(defn delete-objects
-  ([object-keys] (delete-objects default-bucket object-keys))
-  ([bucket-name object-keys]
-   (s3/delete-objects {:bucket-name bucket-name
-                       :quiet false ;; returns deleted objects with response
-                       :keys object-keys})))
+      (:tempfile file)
+      (s3-util/upload-file-to-s3 ctx* (:tempfile file))
 
-(comment
-  (def object-keys ["524bc106-1f0d-44a0-b222-923505264c47/pika.webp"
-                    "524bc106-1f0d-44a0-b222-923505264c47/boop-1.png"])
-  (delete-objects object-keys))
+      :else (throw (Exception. "Unsupported file format")))))
 
-(defn list-app-objects
-  ([prefix] (list-app-objects default-bucket prefix))
-  ([bucket-name prefix]
-   (loop [all-objects []
-          continuation-token nil]
-     (let [opts (cond-> {:bucket-name bucket-name :prefix prefix}
-                  continuation-token
-                  (assoc :continuation-token continuation-token))
-           {:keys [object-summaries next-continuation-token truncated?]}
-           (list-objects-v2 opts)]
-       (if (and truncated? (< (count all-objects) 50000))
-         (recur (into all-objects object-summaries) next-continuation-token)
-         (into all-objects object-summaries))))))
+(defn format-object [{:keys [key object-metadata]}]
+  (-> object-metadata
+      (select-keys [:content-disposition :content-type :content-length :etag])
+      (assoc :size (:content-length object-metadata)
+             :last-modified (-> object-metadata :last-modified .getMillis)
+             :path (object-key->path key))))
 
-(comment
-  (def app-id  #uuid "524bc106-1f0d-44a0-b222-923505264c47")
-  (list-app-objects default-bucket app-id)
-  (list-app-objects app-id))
+(defn get-object-metadata [app-id path]
+  (let [object-key (->object-key app-id path)]
+    (format-object (s3-util/get-object object-key))))
 
-(defn generate-presigned-url
-  ([opts]
-   (let [access-key (config/s3-storage-access-key)
-         secret-key (config/s3-storage-secret-key)]
-     (if (and access-key secret-key)
-       (s3/generate-presigned-url {:access-key access-key
-                                   :secret-key secret-key} opts)
-       ;; For OSS developers, use the default credentials provider chain
-       ;; so they don't need to set up separate storage credentials
-       (s3/generate-presigned-url opts)))))
+(defn bulk-delete-files! [app-id filenames]
+  (let [keys (mapv (fn [filename] (->object-key app-id filename)) filenames)]
+    (s3-util/delete-objects keys)))
 
-(defn signed-upload-url
-  ([object-key] (signed-upload-url default-bucket object-key))
-  ([bucket-name object-key]
-   (generate-presigned-url {:method :put
-                            :bucket-name bucket-name
-                            :key object-key})))
+(defn create-signed-download-url! [app-id filename]
+  (let [expiration (+ (System/currentTimeMillis) (* 1000 60 60 24 7)) ;; 7 days
+        object-key (->object-key app-id filename)]
+    (str (s3-util/generate-presigned-url
+          {:method :get
+           :bucket-name s3-util/default-bucket
+           :key object-key
+           :expiration expiration}))))
 
-(defn signed-download-url
-  ([object-key] (let [expiration (+ (System/currentTimeMillis) (* 1000 60 60 24 7))] ;; 7 days
-                  (signed-download-url default-bucket object-key expiration)))
-  ([object-key expiration] (signed-download-url default-bucket object-key expiration))
-  ([bucket-name object-key expiration]
-   (generate-presigned-url {:method :get
-                            :bucket-name bucket-name
-                            :key object-key
-                            :expiration expiration})))
+;; S3 Usage Metrics
+;; These functions calculate usage by talking to S3 directly. We can use these
+;; for debugging whenever we suspect that our usage metrics based on triples
+;; are off.
+;; ----------------------
 
-(defn upload-image-to-s3
-  ([object-key image-url] (upload-image-to-s3 default-bucket object-key image-url))
-  ([bucket-name object-key image-url]
-   (let [response (clj-http/get image-url {:as :byte-array})
-         image-bytes (:body response)]
-     (s3/put-object {:bucket-name bucket-name
-                     :key object-key
-                     :input-stream (io/input-stream image-bytes)
-                     :metadata {:content-length (count image-bytes)
-                                :content-type "image/png"}}))))
+(defn list-all-app-objects []
+  (loop [all-objects []
+         continuation-token nil]
+    (let [opts (if continuation-token
+                 {:continuation-token continuation-token}
+                 {})
+          {:keys [object-summaries next-continuation-token truncated?]}
+          (s3-util/list-objects opts)]
+      (if truncated?
+        (recur (into all-objects object-summaries) next-continuation-token)
+        (into all-objects object-summaries)))))
+
+(defn list-objects-by-app []
+  (group-by #(object-key->app-id (:key %)) (list-all-app-objects)))
+
+(defn calculate-app-metrics []
+  (let [objects-by-app-id (list-objects-by-app)]
+    (reduce (fn [acc [app-id objects]]
+              (assoc acc app-id {:total-byte-size (reduce (fn [acc obj] (+ acc (:size obj))) 0 objects)
+                                 :total-file-count (count objects)}))
+            {} objects-by-app-id)))
 
 (comment
-  (def image-url "https://i.redd.it/bugxrdkjmm1b1.png")
-  (def app-id  #uuid "524bc106-1f0d-44a0-b222-923505264c47")
-  (def filename "calvin.png")
-  (def object-key (str app-id "/" filename))
-  (def expiration (+ (System/currentTimeMillis) (* 1000 60 60 24)))
-  (upload-image-to-s3 object-key image-url)
-  (get-object object-key)
-  (signed-download-url default-bucket object-key expiration)
-  (signed-download-url object-key expiration)
-  (signed-download-url object-key)
-  (delete-object default-bucket object-key))
+  (count (list-all-app-objects))
+  (list-objects-by-app)
+  (calculate-app-metrics))
