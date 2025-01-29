1b4643bf47b2f45a48ea869fd22184b6c5f6c1b7
Joe Averbukh <joeaverbukh@gmail.com>
2025-01-29 10:09:58 -0800

    Index app_id in app_uploads_table

--- server/resources/migrations/49_add_app_upload_urls.up.sql
+++ server/resources/migrations/49_add_app_upload_urls.up.sql
@@ -5,3 +5,5 @@ CREATE TABLE app_upload_urls (
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   expired_at TIMESTAMP NOT NULL DEFAULT NOW() + INTERVAL '5 minutes'
 );
+
+create index on app_upload_urls(app_id);
