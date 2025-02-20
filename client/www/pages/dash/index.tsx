32a0367a4650f9b49f9bdf42290bf0941ead2614
Joe Averbukh <joeaverbukh@gmail.com>
2025-02-19 17:20:41 -0800

    Prettier lint

--- client/www/pages/dash/index.tsx
+++ client/www/pages/dash/index.tsx
@@ -477,10 +477,7 @@ function Dashboard() {
                 {tab === 'home' ? (
                   <Home />
                 ) : tab === 'explorer' ? (
-                  <ExplorerTab
-                    appId={appId}
-                    db={connection.db}
-                  />
+                  <ExplorerTab appId={appId} db={connection.db} />
                 ) : tab === 'repl' ? (
                   <QueryInspector
                     className="flex-1 w-full"
@@ -833,21 +830,11 @@ function Home() {
   );
 }
 
-function ExplorerTab({
-  db,
-  appId,
-}: {
-  db: InstantReactClient;
-  appId: string;
-}) {
+function ExplorerTab({ db, appId }: { db: InstantReactClient; appId: string }) {
   return (
     <div className="flex-1 overflow-hidden flex flex-col">
       <div className="flex flex-1 flex-col overflow-hidden">
-        <Explorer
-          db={db}
-          appId={appId}
-          key={db._core._reactor.config.appId}
-        />
+        <Explorer db={db} appId={appId} key={db._core._reactor.config.appId} />
       </div>
     </div>
   );
