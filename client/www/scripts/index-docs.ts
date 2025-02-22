9b43747fdeae20ff14771d88cd8711aa2199a61c
Stepan Parunashvili <stepan.p@gmail.com>
2025-02-21 18:07:56 -0800

    [index-docs] use typescript (#922)

--- client/www/scripts/index-docs.ts
+++ client/www/scripts/index-docs.ts
@@ -11,7 +11,7 @@ import algoliasearch from 'algoliasearch';
 import { slugifyWithCounter } from '@sindresorhus/slugify';
 
 import navigation from '../data/docsNavigation.js';
-import Markdoc from '@markdoc/markdoc';
+import Markdoc, { Node } from '@markdoc/markdoc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -25,10 +25,34 @@ const CONFIG = {
   FILE_PATH: path.join(__dirname, 'algolia-objects.json'),
 };
 
+type AlgoliaObject = {
+  content: string;
+  type: 'lvl1' | 'lvl2';
+  hierarchy: {
+    lvl0?: string;
+    lvl1?: string;
+  };
+  url: string;
+  objectID: string;
+};
+
+type Section = {
+  title: string;
+  hash?: string;
+  content: string[];
+};
+
+type PageInfo = {
+  groupTitle: string;
+  pageTitle: string;
+  href: string;
+  sections: Section[];
+};
+
 async function main() {
   const args = process.argv.slice(2);
   const isDryRun = args.includes('--dry-run');
-  const objects = toAlgoliaObjects(extractPages());
+  const objects = toAlgoliaObjects(getPageInfos());
   if (isDryRun) {
     saveToFile(objects);
   } else {
@@ -41,10 +65,11 @@ main().catch((err) => {
   process.exit(1);
 });
 
-async function saveToAlgolia(objects) {
+async function saveToAlgolia(objects: AlgoliaObject[]) {
   if (!CONFIG.ALGOLIA_API_KEY) {
     throw new Error('Export ALGOLIA_API_KEY to run the script.');
   }
+
   const client = algoliasearch(CONFIG.ALGOLIA_APP_ID, CONFIG.ALGOLIA_API_KEY);
   const index = client.initIndex(CONFIG.ALGOLIA_INDEX_NAME);
 
@@ -55,7 +80,7 @@ async function saveToAlgolia(objects) {
   console.log('Indexed', objects.length, 'objects to Algolia');
 }
 
-function saveToFile(objects) {
+function saveToFile(objects: AlgoliaObject[]) {
   console.log('Saving objects to file:', CONFIG.FILE_PATH);
   fs.writeFileSync(CONFIG.FILE_PATH, JSON.stringify(objects, null, 2));
   console.log('Saved', objects.length, 'objects to file');
@@ -64,7 +89,7 @@ function saveToFile(objects) {
 // ---------
 // toAlgoliaObjects
 
-function toAlgoliaObjects(pages) {
+function toAlgoliaObjects(pages: PageInfo[]): AlgoliaObject[] {
   const objects = pages.flatMap(({ groupTitle, pageTitle, href, sections }) => {
     const [rootSection, ...restSections] = sections;
     const rootObject = {
@@ -76,11 +101,11 @@ function toAlgoliaObjects(pages) {
       type: 'lvl1',
       objectID: href,
       url: href,
-    };
+    } satisfies AlgoliaObject;
     const restObjects = restSections.map(
       ({ title: subHeading, hash, content }) => {
-        const url = `${href}#${hash}`;
-        return {
+        const url = `${href}#${hash || ''}`;
+        const obj = {
           content: content.join('\n'),
           type: 'lvl1',
           hierarchy: {
@@ -89,7 +114,8 @@ function toAlgoliaObjects(pages) {
           },
           url,
           objectID: url,
-        };
+        } satisfies AlgoliaObject;
+        return obj;
       },
     );
     return [rootObject, ...restObjects];
@@ -98,7 +124,7 @@ function toAlgoliaObjects(pages) {
   return objects;
 }
 
-function extractPages() {
+function getPageInfos(): PageInfo[] {
   return navigation.flatMap(({ title: groupTitle, links }) => {
     return links.map(({ title: pageTitle, href }) => {
       const mdPath = href === '/docs' ? '/docs/index' : href;
@@ -118,18 +144,17 @@ function extractPages() {
   });
 }
 
-function extractSections(ast, pageTitle) {
+function extractSections(ast: Node, pageTitle: string): Section[] {
   slugify.reset();
 
-  const rootSection = {
+  const rootSection: Section = {
     title: pageTitle,
-    hash: null,
     content: [],
   };
 
   const sections = [rootSection];
 
-  function walk(node) {
+  function walk(node: Node) {
     if (node.type === 'heading' && node.attributes.level <= 2) {
       const title = textContent(node);
       let hash = node.attributes?.id ?? slugify(title);
@@ -140,13 +165,13 @@ function extractSections(ast, pageTitle) {
     if (node.type === 'heading' || node.type === 'paragraph') {
       const content = textContent(node);
 
-      sections.at(-1).content.push(content);
+      sections.at(-1)!.content.push(content);
       return;
     }
 
     if (node.type === 'list') {
       const contents = node.children.map((child) => textContent(child));
-      sections.at(-1).content.push(...contents);
+      sections.at(-1)!.content.push(...contents);
     }
 
     if ('children' in node) {
@@ -162,7 +187,7 @@ function extractSections(ast, pageTitle) {
   return sections;
 }
 
-function textContent(node) {
+function textContent(node: Node) {
   let str = '';
   if (node.type === 'text' && typeof node.attributes?.content === 'string') {
     str += node.attributes.content;
