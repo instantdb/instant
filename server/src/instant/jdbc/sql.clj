80f89b30b36ed00cae7ec68674ff6f7040ad3bf5
Daniel Woelfel <dwwoelfel@gmail.com>
2025-02-26 16:50:58 -0800

    fix merge conflict

--- server/src/instant/jdbc/sql.clj
+++ server/src/instant/jdbc/sql.clj
@@ -133,7 +133,7 @@
   (reduce (fn [acc {:keys [setting value]}]
             (assoc acc (str "postgres-config." setting) value))
           {}
-          postgres-config)))
+          postgres-config))
 
 (defn- span-attrs [conn query tag additional-opts]
   (let [pool-stats (if (instance? HikariDataSource conn)
