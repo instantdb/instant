cb8ada43190a8cdef77f5cafefa19ae327b934cb
Daniel Woelfel <dwwoelfel@gmail.com>
2025-02-27 19:38:20 -0800

    Faster join CTE (#959)

--- server/src/instant/db/datalog.clj
+++ server/src/instant/db/datalog.clj
@@ -1485,13 +1485,22 @@
                                    :table (:from query)}))
                       (let [join-sym (get-in pattern-group [:children :join-sym])
                             join-cte [(kw prefix next-idx)
-                                      {:select [[[:distinct :entity-id] (kw prefix next-idx :-entity-id)]]
-                                       :from [:triples (kw prefix (dec next-idx))]
-                                       :where (list* :and
-                                                     [:= :app-id app-id]
-                                                     (join-conds prefix
-                                                                 symbol-map
-                                                                 {:e [:variable join-sym]}))}
+                                      (let [conds (join-conds prefix
+                                                              symbol-map
+                                                              {:e [:variable join-sym]})]
+                                        (if-let [single-field (when (and (= 1 (count conds))
+                                                                         (= [:= :entity-id]
+                                                                            (take 2 (first conds))))
+                                                                (last (first conds)))]
+                                          ;; If we're only joining on a single col, we can just grab
+                                          ;; that col directly from the CTE
+                                          {:select [[[:distinct single-field] (kw prefix next-idx :-entity-id)]]
+                                           :from (kw prefix (dec next-idx))}
+                                          {:select [[[:distinct :entity-id] (kw prefix next-idx :-entity-id)]]
+                                           :from [:triples (kw prefix (dec next-idx))]
+                                           :where (list* :and
+                                                         [:= :app-id app-id]
+                                                         conds)}))
                                       :materialized]
                             child-res (accumulate-nested-match-query (-> next-acc
                                                                          (update :ctes conj join-cte)
