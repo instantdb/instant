a8353dcc7bdd231981ccbccb2f8e6c80fe55af97
stopachka <stepan.p@gmail.com>
2025-03-23 19:40:23 -0600

    roar

--- server/src/instant/sendgrid.clj
+++ server/src/instant/sendgrid.clj
@@ -1,23 +1,33 @@
-
 (ns instant.sendgrid
   (:require
    [clj-http.client :as clj-http]
    [clojure.data.json :as json]
-   [clojure.tools.logging :as log]
-   [instant.config :as config]))
+   [instant.config :as config]
+   [instant.util.tracer :as tracer]))
 
 (defn send! [{:keys [from to cc bcc subject html]}]
-  (let [sendgrid-body {:personalizations [{:to to :cc cc :bcc bcc}]
-                       :from from
-                       :reply_to {:email "hello@js.ventures"}
-                       :subject subject
-                       :content
-                       [{:type "text/html" :value html}]}]
-    (log/infof "[mail] sending content=%s" sendgrid-body)
-    (clj-http/post
-     "https://api.sendgrid.com/v3/mail/send"
-     {:headers {"Authorization" (str "Bearer " (config/sendgrid-token))
-                "Content-Type" "application/json"}
-      :body (json/write-str sendgrid-body)})))
+  (let [body {:personalizations [{:to to :cc cc :bcc bcc}]
+              :from from
+              :reply_to {:email "hello@js.ventures"}
+              :subject subject
+              :content
+              [{:type "text/html" :value html}]}]
+
+    (if-not (config/sendgrid-send-disabled?)
+      (tracer/with-span! {:name "sendgrid/send-disabled"
+                          :attributes body}
+        (tracer/record-info!
+         {:name "sendgrid-disabled"
+          :attributes
+          {:msg
+           "Sendgrid is disabled, add sendgrid-token to config to enable"}}))
+      (tracer/with-span!
+        {:name "sendgrid/send"
+         :attributes {:body body}}
+        (clj-http/post
+         "https://api.sendgrid.com/v3/mail/send"
+         {:headers {"Authorization" (str "Bearer " (config/sendgrid-token))
+                    "Content-Type" "application/json"}
+          :body (json/write-str body)})))))
 
 (comment)
