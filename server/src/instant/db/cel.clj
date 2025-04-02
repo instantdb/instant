5dd6a61e8be3c39893c25de65afb8c900da08fb8
Daniel Woelfel <dwwoelfel@gmail.com>
2025-04-01 19:46:48 -0700

    Revert "[hotfix] add newData back to view-delete rules (#1044)" (#1059)

--- server/src/instant/db/cel.clj
+++ server/src/instant/db/cel.clj
@@ -319,7 +319,6 @@
 
 (def ^:private cel-view-delete-compiler
   (-> (runtime-compiler-builder)
-      (.addVar "newData" type-obj)
       (.build)))
 
 (def ^:private ^CelCompiler cel-create-update-compiler
