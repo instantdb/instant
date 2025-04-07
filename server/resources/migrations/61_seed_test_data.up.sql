255b7414ddaa558084da5bd084580e1606df320b
stopachka <stepan.p@gmail.com>
2025-04-07 14:09:31 -0700

    roar

--- server/resources/migrations/61_seed_test_data.up.sql
+++ server/resources/migrations/61_seed_test_data.up.sql
@@ -6,22 +6,4 @@ values
     'testuser@instantdb.com',
     '2023-01-23 09:48:35.941053',
     null
-  ),
-  (
-    '6f0f64c9-207f-4b35-aec7-82739dcde58e',
-    'stopa@instantdb.com',
-    '2024-09-10 00:01:36.000999',
-    106722728518978660837
-  ),
-  (
-    'dda67f26-6962-479e-b476-60e4b6963b74',
-    'hello+ephemeralappsdev@instantdb.com',
-    '2024-04-08 17:20:17.644783',
-    null
-  ),
-  (
-    'ee25bd4e-b968-4c1c-bde4-df046ef0dade',
-    'hello+ephemeralapps@instantdb.com',
-    '2024-04-08 17:20:42.704454',
-    null
-  ) on conflict (id) do nothing;
+  ) on conflict do nothing;
