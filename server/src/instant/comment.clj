a099d01585758ec6c82ea87422b3fd3559d4af53
stopachka <stepan.p@gmail.com>
2025-04-07 16:10:11 -0700

    roar

--- server/src/instant/comment.clj
+++ server/src/instant/comment.clj
@@ -2,10 +2,11 @@
 
 (defn zeneca-app!
   []
-  (let [{:keys [id] :as z} (instant.dash.ephemeral-app/create! {:title "c-zeneca"})]
-    (instant.data.bootstrap/add-zeneca-to-app! id)
+  (let [{:keys [id] :as z} ((resolve 'instant.dash.ephemeral-app/create!)
+                            {:title "c-zeneca"})]
+    ((resolve 'instant.data.bootstrap/add-zeneca-to-app!) id)
     z))
 
 (defn empty-app!
   []
-  (instant.dash.ephemeral-app/create! {:title "c-empty-app"}))
+  ((resolve 'instant.dash.ephemeral-app/create!) {:title "c-empty-app"}))
