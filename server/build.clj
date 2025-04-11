d19a44083ec7b57bc4bc397b593301790aef15bb
Daniel Woelfel <dwwoelfel@gmail.com>
2025-04-11 11:09:09 -0700

    allow us to compile java

--- server/build.clj
+++ server/build.clj
@@ -7,11 +7,34 @@
 ;; delay to defer side effects (artifact downloads)
 (def basis (delay (b/create-basis {:project "deps.edn"})))
 
+(defn compile-java [_]
+  (println "COMPILE JAVA")
+  (b/javac {:src-dirs ["src/java"]
+            :class-dir "target/classes"
+            :basis @basis
+            :javac-opts ["-proc:none"]}))
+
+(defn dev [_]
+  (compile-java nil)
+  (b/process
+   (b/java-command {:basis (b/create-basis {:project "deps.edn"
+                                            :aliases ["dev"]})
+                    :main 'clojure.main
+                    :main-args ["-m" "instant.core"]})))
+
+(defn run [_]
+  (compile-java nil)
+  (b/process
+   (b/java-command {:basis (b/create-basis {:project "deps.edn"})
+                    :main 'clojure.main
+                    :main-args ["-m" "instant.core"]})))
+
 (defn clean [_]
   (b/delete {:path "target"}))
 
 (defn uber [_]
   (clean nil)
+  (compile-java)
   (b/copy-dir {:src-dirs ["src" "resources" "data"]
                :target-dir class-dir})
   (b/compile-clj {:basis @basis
