caf5a91f98d32cbf5203544b2d4c629e061c9320
stopachka <stepan.p@gmail.com>
2025-05-06 17:49:46 -0700

    add vanilla js nuxt example

---
+++ client/sandbox/vanilla-js-nuxt/app.vue
@@ -0,0 +1,32 @@
+<template>
+  <button @click="triggerTest">call ID endpoint</button>
+  <p><b>Server response:</b></p>
+  <p>{{ JSON.stringify(response, null, 2) }}</p>
+</template>
+
+<script setup lang="ts">
+import { id } from '@instantdb/core'
+import { v4 } from 'uuid'
+
+const response = ref(null)
+
+async function triggerTest() {
+  console.log('client-side v4:', v4)
+  console.log('client-side instantdb id:', id)
+
+  console.log('client-side uuid v4():', v4())
+  console.log('client-side instantdb id():', id())
+
+  response.value = await $fetch('/api/createId')
+  console.log('server endpoint response:', response.value)
+}
+</script>
+
+<style>
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 90vh;
+}
+</style>
---
+++ client/sandbox/vanilla-js-nuxt/nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  devServer: {
+    port: 3060,
+  },
+});
