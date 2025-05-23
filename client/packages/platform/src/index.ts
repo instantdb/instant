56d68bf45004c4a8a0257d0dd6767fbbefed258d
Daniel Woelfel <dwwoelfel@gmail.com>
2025-05-23 09:17:21 -0700

    Adds oauth flow to platform SDK (#1217)

--- client/packages/platform/src/index.ts
+++ client/packages/platform/src/index.ts
@@ -1,12 +1,23 @@
 import {
+  type InstantDBOAuthAccessToken,
+  type OAuthHandlerConfig,
+  OAuthHandler,
+  InstantOAuthError,
+} from './oauth.ts';
+import { generatePermsTypescriptFile } from './perms.ts';
+import {
   type InstantAPIPlatformSchema,
   generateSchemaTypescriptFile,
 } from './schema.ts';
-import { generatePermsTypescriptFile } from './perms.ts';
+
 import version from './version.js';
 
 export {
   type InstantAPIPlatformSchema,
+  type InstantDBOAuthAccessToken,
+  type OAuthHandlerConfig,
+  OAuthHandler,
+  InstantOAuthError,
   generateSchemaTypescriptFile,
   generatePermsTypescriptFile,
   version,
