8bd005995e849b467f90239d7b662d16e530c91f
Daniel Woelfel <dwwoelfel@gmail.com>
2025-06-10 14:34:42 -0700

    Set `required` on one-to-one required attrs

--- client/packages/platform/src/api.ts
+++ client/packages/platform/src/api.ts
@@ -351,6 +351,8 @@ function apiSchemaAttrToLinkDef(attr: InstantDBAttr) {
       on: re,
       has: rhas,
       label: rlabel,
+      required:
+        (attr['required?'] && fhas === 'one' && rhas === 'one') || undefined,      
       onDelete:
         attr['on-delete-reverse'] === 'cascade'
           ? ('cascade' as 'cascade')
