4e3d7897da0124fa35149ed58d30f2e7091ab297
Daniel Woelfel <dwwoelfel@gmail.com>
2025-07-16 14:53:46 -0700

    fix formatting

--- client/www/pages/intern/index.tsx
+++ client/www/pages/intern/index.tsx
@@ -37,8 +37,7 @@ const tools: ToolCard[] = [
   {
     title: 'Paid Subscriptions',
     href: '/intern/paid',
-    description:
-      'Shows data about our paid apps.',
+    description: 'Shows data about our paid apps.',
     category: 'Analytics',
   },
   {
