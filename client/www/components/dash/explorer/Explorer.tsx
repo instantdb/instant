1192ff77f5a6e20f2b9e98442268902e30e63cc8
Joe Averbukh <joeaverbukh@gmail.com>
2025-07-23 10:19:07 -0700

    [Exporer] Autofocus create namespace input (#1418)

--- client/www/components/dash/explorer/Explorer.tsx
+++ client/www/components/dash/explorer/Explorer.tsx
@@ -1622,7 +1622,12 @@ function NewNamespaceDialog({
         Create a new namespace
       </h5>
 
-      <TextInput label="Name" value={name} onChange={(n) => setName(n)} />
+      <TextInput
+        label="Name"
+        value={name}
+        onChange={(n) => setName(n)}
+        autoFocus
+      />
 
       <ActionButton
         type="submit"
