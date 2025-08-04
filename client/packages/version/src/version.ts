4b63231454e00a172d4028bf6495df278422473e
Daniel Woelfel <dwwoelfel@gmail.com>
2025-08-04 16:40:44 -0700

    Publish v0.20.19 with fix for date format

--- client/packages/version/src/version.ts
+++ client/packages/version/src/version.ts
@@ -1,6 +1,6 @@
 // This is the shared version for all of the js packages
 // Update the version here and merge your code to main to
 // publish a new version of all of the packages to npm.
-const version = 'v0.20.17';
+const version = 'v0.20.18';
 
 export { version };
