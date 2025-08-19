b6b2b4482bf260256a33d8be50b9a6712f458296
Stepan Parunashvili <stepan.p@gmail.com>
2025-08-18 17:49:35 -0700

    [docs] fix docs/schema (#1536)

--- client/sandbox/react-native-expo/instant.schema.ts
+++ client/sandbox/react-native-expo/instant.schema.ts
@@ -1,11 +1,9 @@
-// Docs: https://www.instantdb.com/docs/schema
+// Docs: https://www.instantdb.com/docs/modeling-data
 
 import { i } from '@instantdb/react-native';
 
 const _schema = i.schema({
   // This section lets you define entities: think `posts`, `comments`, etc
-  // Take a look at the docs to learn more:
-  // https://www.instantdb.com/docs/schema#defining-entities
   entities: {
     $users: i.entity({
       email: i.string().unique().indexed(),
@@ -13,11 +11,8 @@ const _schema = i.schema({
   },
   // You can define links here.
   // For example, if `posts` should have many `comments`.
-  // More in the docs:
-  // https://www.instantdb.com/docs/schema#defining-links
   links: {},
   // If you use presence, you can define a room schema here
-  // https://www.instantdb.com/docs/schema#defining-rooms
   rooms: {},
 });
 
