705c51da6144ce2bb5729a3c395aee1be158a10b
Daniel Woelfel <dwwoelfel@gmail.com>
2025-08-29 10:34:18 -0700

    fix

--- server/src/instant/jdbc/sql.clj
+++ server/src/instant/jdbc/sql.clj
@@ -381,7 +381,14 @@
                       (let [debug-info (str "-- trace-id=" trace-id
                                             ", span-id=" span-id
                                             "\n")]
-                        (str debug-info s))))
+                        (if-not (string/starts-with? s "/*+")
+                          (str debug-info s)
+                          (let [end-comment (or (when-let [i (string/index-of s "*/")]
+                                                  (+ i 2))
+                                                (count s))]
+                            (str (subs s 0 end-comment)
+                                 debug-info
+                                 (subs s end-comment)))))))
     query))
 
 (defn apply-postgres-config [postgres-config created-connection? ^Connection c]
