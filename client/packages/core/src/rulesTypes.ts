9fa161959c196baf1bf0a3afc77cd7ac34b9a1b7
Nikita Prokopov <niki@tonsky.me>
2025-09-03 17:22:56 +0200

    Remove inferring from schema per @stopa request

--- client/packages/core/src/rulesTypes.ts
+++ client/packages/core/src/rulesTypes.ts
@@ -8,29 +8,19 @@ type InstantRulesAttrsAllowBlock = {
   delete?: string | null | undefined;
 };
 
-export type InstantRulesAllowBlock<
-  Schema extends InstantSchemaDef<any, any, any> = InstantUnknownSchema,
-> = InstantRulesAttrsAllowBlock & {
-  link?:
-    | { [EntityName in keyof Schema['entities']]?: string }
-    | null
-    | undefined;
-  unlink?:
-    | { [EntityName in keyof Schema['entities']]?: string }
-    | null
-    | undefined;
+export type InstantRulesAllowBlock = InstantRulesAttrsAllowBlock & {
+  link?: { [key: string]: string } | null | undefined;
+  unlink?: { [key: string]: string } | null | undefined;
 };
-
-export type InstantRulesAllowBlockWithoutLinks = InstantRulesAttrsAllowBlock;
 
 export type InstantRules<
   Schema extends InstantSchemaDef<any, any, any> = InstantUnknownSchema,
 > = {
-  $default?: { bind?: string[]; allow: InstantRulesAllowBlock<Schema> };
-  attrs?: { bind?: string[]; allow: InstantRulesAllowBlock<Schema> };
+  $default?: { bind?: string[]; allow: InstantRulesAllowBlock };
+  attrs?: { bind?: string[]; allow: InstantRulesAttrsAllowBlock };
 } & {
   [EntityName in keyof Schema['entities']]: {
     bind?: string[];
-    allow: InstantRulesAllowBlock<Schema>;
+    allow: InstantRulesAllowBlock;
   };
 };
