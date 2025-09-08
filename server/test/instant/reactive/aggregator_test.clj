55ca9c822a26a979cdc47c32ae44748789e602cc
Daniel Woelfel <dwwoelfel@gmail.com>
2025-09-08 11:02:52 -0700

    more debug info

--- server/test/instant/reactive/aggregator_test.clj
+++ server/test/instant/reactive/aggregator_test.clj
@@ -28,7 +28,7 @@
 
 (defn check-sketches [app r]
   (let [triples (sql/select (aurora/conn-pool :read)
-                            ["select * from triples where app_id = ?" (:id app)])
+                            ["select *, pg_column_size(triples) actual_pg_size from triples where app_id = ?" (:id app)])
         attr-groups (group-by :attr_id triples)
         value-groups (group-by #(select-keys % [:attr_id :value :checked_data_type]) triples)
         reverse-value-groups (group-by #(select-keys % [:attr_id :entity_id])
@@ -62,12 +62,18 @@
           (str "count mismatch for reverse ref " (resolvers/->friendly r attr_id) " entity_id=" entity_id)))
 
     (testing "triples_pg_size is correct"
+      (is (= []
+             (take 100 (keep (fn [t]
+                               (when (not= (:pg_size t)
+                                           (:actual_pg_size t))
+                                 (select-keys t [:attr_id :entity_id :value :pg_size :actual_pg_size])))
+                             triples))))
       (doseq [attr (attr-model/get-by-app-id (:id app))]
         (tool/def-locals)
         (is (= (:sum (sql/select-one (aurora/conn-pool :read)
-                                    ["select sum(pg_column_size(triples)) sum from triples where app_id = ?::uuid and attr_id = ?::uuid"
-                                     (:id app)
-                                     (:id attr)]))
+                                     ["select sum(pg_column_size(triples)) sum from triples where app_id = ?::uuid and attr_id = ?::uuid"
+                                      (:id app)
+                                      (:id attr)]))
                (get-in sketches [(:id attr) :triples-pg-size]))
             (str "pg_size mismatch for " (resolvers/->friendly r (:id attr))))))))
 
