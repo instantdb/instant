f29ddabf3b5948ff511a9f0466a6880f8f37f3d9
Daniel Woelfel <dwwoelfel@gmail.com>
2025-09-09 14:40:48 -0700

    docstring

--- server/src/instant/model/app.clj
+++ server/src/instant/model/app.clj
@@ -379,7 +379,7 @@
 
   Usage is comprised of both raw data and overhead data (indexes, toast tables, etc.).
 
-  sum(pg_column_size(t)) calculates the total data size for the specified app_id.
+  sum(pg_triples_size) calculates the total data size for the specified app_id.
   pg_total_relation_size('triples') / pg_relation_size('triples') calculates
   the ratio of the total table size to the actual data size. This ratio
   represents the overhead factor.
