e239f0609b1fd196f91590812383c3d63922cdc4
Joe Averbukh <joeaverbukh@gmail.com>
2025-09-11 18:03:34 -0700

    sm

--- client/www/pages/index.tsx
+++ client/www/pages/index.tsx
@@ -102,10 +102,6 @@ function CreateInstantApp() {
     >
       <div
         className="relative bg-transparent border border-black font-mono text-lg cursor-pointer hover:bg-gray-50/30 transition-colors inline-flex items-center px-4 py-2 w-full"
-        onClick={() => {
-          navigator.clipboard.writeText(command);
-        }}
-        title="Click to copy"
       >
         <div className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-800 transition-colors">
           {showCopySuccess ? (
