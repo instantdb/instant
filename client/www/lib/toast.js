a34dd26686867b193a88a120848ae0364ca77327
Joe Averbukh <joeaverbukh@gmail.com>
2025-09-12 16:23:24 -0700

    Add toast dark mode styles

--- client/www/lib/toast.js
+++ client/www/lib/toast.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { ToastContainer, toast, Flip } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { InformationCircleIcon } from '@heroicons/react/24/outline';
@@ -5,6 +6,7 @@ import {
   ExclamationCircleIcon,
   CheckCircleIcon,
 } from '@heroicons/react/24/solid';
+import { useDarkMode } from '@/lib/hooks/useDarkMode';
 
 export function infoToast(text, options) {
   toast.info(text, {
@@ -16,7 +18,13 @@ export function infoToast(text, options) {
 
 export function successToast(text, options) {
   toast.success(text, {
-    icon: <CheckCircleIcon className="text-green-500" width={20} height={20} />,
+    icon: (
+      <CheckCircleIcon
+        className="text-green-500 dark:text-green-400"
+        width={20}
+        height={20}
+      />
+    ),
     toastId: text,
     ...options,
   });
@@ -25,7 +33,11 @@ export function successToast(text, options) {
 export function errorToast(text, options) {
   toast.error(text, {
     icon: (
-      <ExclamationCircleIcon className="text-red-500" width={20} height={20} />
+      <ExclamationCircleIcon
+        className="text-red-500 dark:text-red-400"
+        width={20}
+        height={20}
+      />
     ),
     toastId: text,
     autoClose: 500000000,
@@ -35,15 +47,16 @@ export function errorToast(text, options) {
 }
 
 export function StyledToastContainer() {
+  const isDarkMode = useDarkMode();
+
   return (
     <ToastContainer
       position="top-right"
       autoClose={3000}
       hideProgressBar
       draggablePercent={30}
-      theme="light"
+      theme={isDarkMode ? 'dark' : 'light'}
       limit={3}
-      closeButton={false}
     />
   );
 }
