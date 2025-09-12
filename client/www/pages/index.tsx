2140bc110353a87a9b5b36510b4bd9c7de6c30f4
Joe Averbukh <joeaverbukh@gmail.com>
2025-09-11 17:57:19 -0700

    Use CopyToClipboard

--- client/www/pages/index.tsx
+++ client/www/pages/index.tsx
@@ -26,6 +26,7 @@ import MuxPlayer from '@mux/mux-player-react';
 import * as muxVideos from '@/lib/muxVideos';
 import useTotalSessionsCount from '@/lib/hooks/useTotalSessionsCount';
 import AnimatedCounter from '@/components/AnimatedCounter';
+import CopyToClipboard from 'react-copy-to-clipboard';
 
 const appId = 'fc5a4977-910a-43d9-ac28-39c7837c1eb5';
 
@@ -88,36 +89,42 @@ function CreateInstantApp() {
   const command = 'npx create-instant-app';
 
   return (
-    <div
-      className="relative bg-transparent border border-black font-mono text-lg cursor-pointer hover:bg-gray-50/30 transition-colors inline-flex items-center px-4 py-2 w-full"
-      onClick={() => {
-        navigator.clipboard.writeText(command);
+    <CopyToClipboard
+      text={command}
+      onCopy={() => {
         setShowCopySuccess(true);
         setTimeout(() => setShowCopySuccess(false), 2000);
       }}
-      title="Click to copy"
     >
-      <div className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-800 transition-colors">
-        <svg
-          className={`w-5 h-5 ${showCopySuccess ? 'text-orange-600' : ''}`}
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d={
-              showCopySuccess
-                ? 'M5 13l4 4L19 7'
-                : 'M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z'
-            }
-          />
-        </svg>
+      <div
+        className="relative bg-transparent border border-black font-mono text-lg cursor-pointer hover:bg-gray-50/30 transition-colors inline-flex items-center px-4 py-2 w-full"
+        onClick={() => {
+          navigator.clipboard.writeText(command);
+        }}
+        title="Click to copy"
+      >
+        <div className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-800 transition-colors">
+          <svg
+            className={`w-5 h-5 ${showCopySuccess ? 'text-orange-600' : ''}`}
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d={
+                showCopySuccess
+                  ? 'M5 13l4 4L19 7'
+                  : 'M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z'
+              }
+            />
+          </svg>
+        </div>
+        $ {command}
       </div>
-      $ {command}
-    </div>
+    </CopyToClipboard>
   );
 }
 
