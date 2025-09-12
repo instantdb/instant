ccadac6a818e12d7b4751277dd19b5deebab2625
Joe Averbukh <joeaverbukh@gmail.com>
2025-09-11 18:01:54 -0700

    Use heroui-icons

--- client/www/pages/index.tsx
+++ client/www/pages/index.tsx
@@ -20,7 +20,11 @@ import {
   Link,
 } from '@/components/marketingUi';
 
-import { ChevronRightIcon } from '@heroicons/react/24/solid';
+import {
+  ChevronRightIcon,
+  ClipboardDocumentIcon,
+  CheckIcon,
+} from '@heroicons/react/24/solid';
 import { useIsHydrated } from '@/lib/hooks/useIsHydrated';
 import MuxPlayer from '@mux/mux-player-react';
 import * as muxVideos from '@/lib/muxVideos';
@@ -104,23 +108,11 @@ function CreateInstantApp() {
         title="Click to copy"
       >
         <div className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-800 transition-colors">
-          <svg
-            className={`w-5 h-5 ${showCopySuccess ? 'text-orange-600' : ''}`}
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d={
-                showCopySuccess
-                  ? 'M5 13l4 4L19 7'
-                  : 'M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z'
-              }
-            />
-          </svg>
+          {showCopySuccess ? (
+            <CheckIcon className="w-5 h-5 text-orange-600" />
+          ) : (
+            <ClipboardDocumentIcon className="w-5 h-5" />
+          )}
         </div>
         $ {command}
       </div>
