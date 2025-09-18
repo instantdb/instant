ce756e7d1e9ee06ad6af02803716643b6db8291c
Joe Averbukh <joeaverbukh@gmail.com>
2025-09-17 17:22:04 -0700

    Fix create-instant-app for expo (#1679)

--- client/packages/create-instant-app/template/base/expo/babel.config.js
+++ client/packages/create-instant-app/template/base/expo/babel.config.js
@@ -1,6 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
   return {
+    plugins: [
+      "react-native-reanimated/plugin",
+    ],
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
