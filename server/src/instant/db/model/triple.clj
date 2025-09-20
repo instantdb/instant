c5a9128767850349a41b770c89ff5933caed6ad7
Daniel Woelfel <dwwoelfel@gmail.com>
2025-09-19 17:45:58 -0700

    Revert "see if setting the id like this works"
    This reverts commit 3d05740eac26413d80bbab88ccf841712274278c.

--- server/src/instant/db/model/triple.clj
+++ server/src/instant/db/model/triple.clj
@@ -277,9 +277,7 @@
         enhanced-lookup-refs
         {:select
          [[[:cast :ilr.app_id :uuid] :app-id]
-          [[:case [:= [:inline "id"] :a.label]
-            [:cast :ilr.value :uuid]
-            :else [:gen_random_uuid]] :entity-id]
+          [[:gen_random_uuid] :entity-id]
           [(hsql-attr-id-or-raise :ilr.attr-id :a.id)
            :attr-id]
           [[:cast :ilr.value :jsonb] :value]
@@ -429,9 +427,7 @@
         enhanced-lookup-refs
         {:select
          [[[:cast :ilr.app_id :uuid] :app-id]
-          [[:case [:= [:inline "id"] :a.label]
-            [:cast :ilr.value :uuid]
-            :else [:gen_random_uuid]] :entity-id]
+          [[:gen_random_uuid] :entity-id]
           [(hsql-attr-id-or-raise :ilr.attr-id :a.id)
            :attr-id]
           [[:cast :ilr.value :jsonb] :value]
