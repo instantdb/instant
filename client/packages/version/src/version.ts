1e0ac41bf95b69438de7a05615d1270b8a9d3fd9
Nikita Prokopov <niki@tonsky.me>
2025-09-30 21:32:55 +0200

    v0.22.0

--- client/packages/version/src/version.ts
+++ client/packages/version/src/version.ts
@@ -1,6 +1,6 @@
 // This is the shared version for all of the js packages
 // Update the version here and merge your code to main to
 // publish a new version of all of the packages to npm.
-const version = 'v0.21.30';
+const version = 'v0.22.0';
 
 export { version };
