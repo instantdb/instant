9be926d4e948da293c98560a1e5a2cab08300113
stopachka <stepan.p@gmail.com>
2025-10-07 10:10:50 -0700

    sm

--- client/www/lib/muxVideos.ts
+++ client/www/lib/muxVideos.ts
@@ -34,7 +34,7 @@ export const walkthrough: MuxPlayerProps = {
 
 export const createInstantApp: MuxPlayerProps = {
   streamType: 'on-demand',
-  playbackId: 'u2TNknudkJF1icAPjgblm7IlwsB1I8UmyXKNv02EWTIQ',
+  playbackId: 'MCcq6QBw6OzNSrt02YpfAH2QP8y0000xapuoaP01IJy02xN4',
   primaryColor: '#FFFFFF',
   secondaryColor: '#000000',
   placeholder: `data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%"><filter id="b" color-interpolation-filters="sRGB"><feGaussianBlur stdDeviation="20"/><feComponentTransfer><feFuncA type="discrete" tableValues="1 1"/></feComponentTransfer></filter><g filter="url(%23b)"><image width="100%" height="100%" preserveAspectRatio="xMidYMid slice" href="data:image/webp;base64,UklGRlgAAABXRUJQVlA4IEwAAAAwAgCdASoQAAoAAQAcJaQC7H8AGBvm9JqyAAD+/mVFyNo3XECtBS+Gp2qGzM7SRSe7VcvFyt53Oecwgr8gIDgQPEFW7dIg+7AtgAAA"/></g></svg>`,
