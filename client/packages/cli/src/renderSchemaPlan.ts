636b8015f36f346f62ff36215738d0ba516c8795
Drew Harris <drew@drewh.net>
2025-10-07 10:00:09 -0700

    brighter colors and delete namespace output

--- client/packages/cli/src/renderSchemaPlan.ts
+++ client/packages/cli/src/renderSchemaPlan.ts
@@ -5,6 +5,7 @@ export const renderSchemaPlan = (planSteps: MigrationTx[]) => {
   const groupedSteps: (
     | MigrationTx
     | { type: 'create-namespace'; namespace: string; attrs: string[] }
+    | { type: 'delete-namespace'; namespace: string; attrs: string[] }
   )[] = [];
 
   let i = 0;
@@ -40,6 +41,35 @@ export const renderSchemaPlan = (planSteps: MigrationTx[]) => {
         groupedSteps.push(step);
         i++;
       }
+    } else if (step.type === 'delete-attr') {
+      const namespace = step.namespace;
+      const namespaceAttrs: string[] = [];
+      let hasIdAttr = false;
+      let j = i;
+
+      while (j < planSteps.length) {
+        const currentStep = planSteps[j];
+        if (currentStep.type !== 'delete-attr') break;
+        if (currentStep.namespace !== namespace) break;
+
+        namespaceAttrs.push(currentStep.attrName);
+        if (currentStep.attrName === 'id') {
+          hasIdAttr = true;
+        }
+        j++;
+      }
+
+      if (hasIdAttr && namespaceAttrs.length > 1) {
+        groupedSteps.push({
+          type: 'delete-namespace',
+          namespace,
+          attrs: namespaceAttrs.filter((name) => name !== 'id'),
+        });
+        i = j;
+      } else {
+        groupedSteps.push(step);
+        i++;
+      }
     } else {
       groupedSteps.push(step);
       i++;
@@ -53,14 +83,19 @@ export const renderSchemaPlan = (planSteps: MigrationTx[]) => {
           `${chalk.bgGreen.black(' + CREATE NAMESPACE ')} ${step.namespace}${step.attrs.length > 0 ? ` (${step.attrs.join(', ')})` : ''}`,
         );
         break;
+      case 'delete-namespace':
+        console.log(
+          `${chalk.bgRed(' - DELETE NAMESPACE ')} ${step.namespace}${step.attrs.length > 0 ? ` (${step.attrs.join(', ')})` : ''}`,
+        );
+        break;
       case 'add-attr':
         console.log(
-          `${chalk.bgGreen.black(' + CREATE ATTR ')} ${step.identifier.namespace}.${step.identifier.attrName}`,
+          `${chalk.bgGreen(' + CREATE ATTR ')} ${step.identifier.namespace}.${step.identifier.attrName}`,
         );
         break;
       case 'delete-attr':
         console.log(
-          `${chalk.bgRed.black(' - DELETE ATTR ')} ${step.namespace}.${step.attrName}`,
+          `${chalk.bgRed(' - DELETE ATTR ')} ${step.namespace}.${step.attrName}`,
         );
         break;
       case 'update-attr':
@@ -70,52 +105,52 @@ export const renderSchemaPlan = (planSteps: MigrationTx[]) => {
             step.partialAttr['forward-identity'].attrName
         ) {
           console.log(
-            `${chalk.bgYellow.black(' * RENAME ATTR ')} ${step.identifier.namespace}.${step.identifier.attrName} -> ${step.identifier.namespace}.${step.partialAttr['forward-identity'].attrName}`,
+            `${chalk.bgYellow(' * RENAME ATTR ')} ${step.identifier.namespace}.${step.identifier.attrName} -> ${step.identifier.namespace}.${step.partialAttr['forward-identity'].attrName}`,
           );
         } else {
           console.log(
-            `${chalk.bgYellow.black(' * UPDATE ATTR ')} ${step.identifier.namespace}.${step.identifier.attrName}`,
+            `${chalk.bgYellow(' * UPDATE ATTR ')} ${step.identifier.namespace}.${step.identifier.attrName}`,
           );
         }
         break;
       case 'index':
         console.log(
-          `${chalk.bgBlue.black(' + CREATE INDEX ')} ${step.namespace}.${step.attrName}`,
+          `${chalk.bgBlue(' + CREATE INDEX ')} ${step.namespace}.${step.attrName}`,
         );
         break;
       case 'remove-index':
         console.log(
-          `${chalk.bgBlue.black(' - DELETE INDEX ')} ${step.namespace}.${step.attrName}`,
+          `${chalk.bgBlue(' - DELETE INDEX ')} ${step.namespace}.${step.attrName}`,
         );
         break;
       case 'required':
         console.log(
-          `${chalk.bgBlue.black(' + MAKE REQUIRED ')} ${step.namespace}.${step.attrName}`,
+          `${chalk.bgBlue(' + MAKE REQUIRED ')} ${step.namespace}.${step.attrName}`,
         );
         break;
       case 'unique':
         console.log(
-          `${chalk.bgBlue.black(' * MAKE UNIQUE ')} ${step.namespace}.${step.attrName}`,
+          `${chalk.bgBlue(' * MAKE UNIQUE ')} ${step.namespace}.${step.attrName}`,
         );
         break;
       case 'remove-required':
         console.log(
-          `${chalk.bgBlue.black(' - MAKE OPTIONAL ')} ${step.namespace}.${step.attrName}`,
+          `${chalk.bgBlue(' - MAKE OPTIONAL ')} ${step.namespace}.${step.attrName}`,
         );
         break;
       case 'remove-unique':
         console.log(
-          `${chalk.bgBlue.black(' - REMOVE UNIQUE CONSTRAINT')} ${step.namespace}.${step.attrName}`,
+          `${chalk.bgBlue(' - REMOVE UNIQUE CONSTRAINT')} ${step.namespace}.${step.attrName}`,
         );
         break;
       case 'check-data-type':
         console.log(
-          `${chalk.bgBlue.black(' + SET DATA TYPE ')} ${step.identity.namespace}.${step.identity.attrName} ${chalk.dim(step['checked-data-type'])}`,
+          `${chalk.bgBlue(' + SET DATA TYPE ')} ${step.identity.namespace}.${step.identity.attrName} ${chalk.dim(step['checked-data-type'])}`,
         );
         break;
       case 'remove-data-type':
         console.log(
-          `${chalk.bgBlue.black(' - REMOVE DATA TYPE CONSTRAINT ')} ${step.namespace}.${step.attrName}`,
+          `${chalk.bgBlue(' - REMOVE DATA TYPE CONSTRAINT ')} ${step.namespace}.${step.attrName}`,
         );
         break;
 
