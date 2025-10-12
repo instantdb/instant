ac48026faa6ec21aacf2483f29212e8aebc54cd2
Daniel Woelfel <dwwoelfel@gmail.com>
2025-10-12 14:19:17 -0700

    cleanup

--- server/src/instant/reactive/sse.clj
+++ server/src/instant/reactive/sse.clj
@@ -6,20 +6,11 @@
    [instant.reactive.store :as rs]
    [instant.util.exception :as ex]
    [instant.util.hazelcast :as h]
-   [medley.core :refer [find-first]])
+   [instant.util.tracer :as tracer])
   (:import
    (com.hazelcast.cluster Member)
-   (com.hazelcast.core ExecutionCallback HazelcastInstance IExecutorService)
+   (com.hazelcast.core HazelcastInstance IExecutorService)
    (java.util UUID)))
-
-(deftype SendMessageCallback [error-promise]
-  ExecutionCallback
-  (onFailure [_ t]
-    (deliver error-promise t))
-  (onResponse [_ v]
-    (if v
-      (deliver error-promise (ex-info (:message v) (:ex-data v)))
-      (deliver error-promise nil))))
 
 (defn send-message-to-member [^Member member
                               ^UUID app-id
@@ -28,21 +19,20 @@
                               message]
   (let [executor (HazelcastInstance/.getExecutorService (eph/get-hz) "sse-send-message")
         ^Callable callable (h/->SSEMessage app-id session-id sse-token-hash message)
-        error-promise (promise)
-        ^ExecutionCallback callback (->SendMessageCallback error-promise)]
-    (IExecutorService/.submitToMember executor
-                                      callable
-                                      member
-                                      callback)
-    (when-let [error @error-promise]
-      (throw error))))
+        instant-error (deref (IExecutorService/.submitToMember executor
+                                                               callable
+                                                               member))]
+    (when instant-error
+      (throw (ex-info (:message instant-error)
+                      (:ex-data instant-error))))))
 
 
 (defn send-message-callable* [^UUID app-id
                               ^UUID session-id
                               ^bytes sse-token-hash
                               message]
-  (session/sse-on-message rs/store {:session-id session-id
+  (session/sse-on-message rs/store {:app-id app-id
+                                    :session-id session-id
                                     :sse-token-hash sse-token-hash
                                     :message message}))
 
@@ -52,18 +42,21 @@
                              ^UUID session-id
                              ^bytes sse-token-hash
                              message]
-  (try
-    (send-message-callable* app-id session-id sse-token-hash message)
-    nil
-    (catch clojure.lang.ExceptionInfo e
-      ;; Reduce the amount of data we send through hz for ordinary errors
-      (if-let [instant-ex ^Exception (ex/find-instant-exception)]
-        {:message (.getMessage instant-ex)
-         :ex-data (ex-data instant-ex)}
-        (throw e)))))
+  (tracer/with-span! {:name "sse/send-message-callable"
+                      :attributes {:app-id app-id
+                                   :session-id session-id}}
+    (try
+      (send-message-callable* app-id session-id sse-token-hash message)
+      nil
+      (catch clojure.lang.ExceptionInfo e
+        ;; Reduce the amount of data we send through hz for ordinary errors
+        (if-let [instant-ex ^Exception (ex/find-instant-exception e)]
+          {:message (.getMessage instant-ex)
+           :ex-data (ex-data instant-ex)}
+          (throw e))))))
 
 (defn skip-hz-in-dev? []
-  (and (= (config/dev?))
+  (and (config/dev?)
        ;; In dev, send half the requests to ourself through hazelcast
        ;; so we test both paths while developing.
        (= (rand-int 2) 1)))
@@ -73,12 +66,17 @@
                        ^UUID session-id
                        ^bytes sse-token-hash
                        message]
-
-  (if (and (= machine-id config/machine-id)
-           (skip-hz-in-dev?))
-    ;; Skip hazelcast if the session lives on our machine.
-    (send-message-callable* app-id session-id sse-token-hash message)
-    (let [member (get eph/hz-member-by-machine-id-cache machine-id)]
-      (when-not member
-        (ex/throw-member-missing! machine-id))
-      (send-message-to-member member app-id session-id sse-token-hash message))))
+  (let [handle-locally? (and (= machine-id config/machine-id)
+                             (skip-hz-in-dev?))]
+    (tracer/with-span! {:name "sse/enqueue-message"
+                        :attributes {:app-id app-id
+                                     :machine-id machine-id
+                                     :session-id session-id
+                                     :handle-locally? handle-locally?}}
+      (if handle-locally?
+          ;; Skip hazelcast if the session lives on our machine.
+          (send-message-callable* app-id session-id sse-token-hash message)
+        (let [member (get eph/hz-member-by-machine-id-cache machine-id)]
+          (when-not member
+            (ex/throw-member-missing! machine-id))
+          (send-message-to-member member app-id session-id sse-token-hash message))))))
