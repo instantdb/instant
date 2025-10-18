1fd6a7612cd8e2904ac39ab91e52775b1eded28b
Daniel Woelfel <dwwoelfel@gmail.com>
2025-10-17 21:01:36 -0700

    self-host fonts

--- client/www/pages/_document.js
+++ client/www/pages/_document.js
@@ -9,14 +9,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
