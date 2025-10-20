93654df1039d3483c04ad07ad426abcfd3d8a7e4
Drew Harris <drew@drewh.net>
2025-10-20 10:56:40 -0700

    add tests for buildAutoRenameSelector

--- client/packages/cli/src/index.js
+++ client/packages/cli/src/index.js
@@ -43,6 +43,7 @@ import { UI } from './ui/index.js';
 import { deferred } from './ui/lib.js';
 import { promptOk } from './util/promptOk.js';
 import { ResolveRenamePrompt } from './util/renamePrompt.js';
+import { buildAutoRenameSelector } from './rename.js';
 
 const execAsync = promisify(exec);
 
@@ -2118,85 +2119,4 @@ async function validateAppLinked() {
     );
     process.exit(1);
   }
-}
-
-/**
- * Build a function to resolve renames from cli rename flags
- * The diffSchemas function takes a fixed amount of arguments so we
- * return a function from a function here
- * @param {*} opts  program arguments from commander
- * @returns
- */
-function buildAutoRenameSelector(opts) {
-  return async function (created, promptData, extraInfo) {
-    if (!opts.rename || !Array.isArray(opts.rename)) {
-      return created;
-    }
-
-    // Parse rename options: format is "from:to"
-    // note that it saves backwards since we will be testing against the base
-    // case of a created attr
-    const renameMap = new Map();
-    for (const renameStr of opts.rename) {
-      const [from, to] = renameStr.split(':');
-      if (from && to) {
-        renameMap.set(to.trim(), from.trim());
-      }
-    }
-
-    let lookupNames = [];
-    if (extraInfo?.type === 'attribute' && extraInfo?.entityName) {
-      lookupNames = [`${extraInfo.entityName}.${created}`];
-    } else if (extraInfo?.type === 'link') {
-      // Extract both forward and reverse parts
-      const parts = created.split('<->');
-      lookupNames = [parts[0], parts[1]];
-    } else {
-      return created;
-    }
-
-    // Try to find a match in the rename map using the lookup names
-    let fromAttr = null;
-    for (const lookupName of lookupNames) {
-      if (renameMap.has(lookupName)) {
-        fromAttr = renameMap.get(lookupName);
-        break;
-      }
-    }
-
-    if (fromAttr) {
-      let fromValue;
-      if (extraInfo?.type === 'attribute') {
-        fromValue = fromAttr.split('.').pop();
-      } else {
-        const matchingItem = promptData.find((item) => {
-          const itemStr = typeof item === 'string' ? item : item.from;
-          const itemParts = itemStr.split('<->');
-          return itemParts[0] === fromAttr || itemParts[1] === fromAttr;
-        });
-
-        if (matchingItem) {
-          fromValue =
-            typeof matchingItem === 'string' ? matchingItem : matchingItem.from;
-        } else {
-          return created;
-        }
-      }
-
-      const hasMatch = promptData.some((item) => {
-        if (typeof item === 'string') {
-          return item === fromValue;
-        } else if (item.from) {
-          return item.from === fromValue;
-        }
-        return false;
-      });
-
-      if (hasMatch) {
-        return { from: fromValue, to: created };
-      }
-    }
-
-    return created;
-  };
 }
