2a054b4626aa9365784c12a449d10739f9bf8793
stopachka <stepan.p@gmail.com>
2025-10-20 17:40:28 -0700

    Revert "[tbilisi] add a note on hackathon (#1843)"
    This reverts commit b8a7600c122ccb2b6fd16c3ad30473b2e11b2af9.

--- client/www/pages/api/geo.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const country = req.headers['x-vercel-ip-country'] || 'unknown';
-
-  res.status(200).json({ country });
-}
