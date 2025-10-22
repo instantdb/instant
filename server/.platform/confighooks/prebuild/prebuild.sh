a2d0b070a66445567f493c1987a4c94b2be403cf
Daniel Woelfel <dwwoelfel@gmail.com>
2025-10-22 08:23:54 -0700

    [staging] make sure we leave 1gb free

--- server/.platform/confighooks/prebuild/prebuild.sh
+++ server/.platform/confighooks/prebuild/prebuild.sh
@@ -2,7 +2,11 @@
 
 set -x
 
-# Rotate Hazelcast port on every deploy
+###################
+# Setup Hazelcast #
+###################
+
+# Rotates hazelcast port on each new deploy
 
 file="/etc/deploy_count.txt"
 
@@ -23,7 +27,9 @@ echo "HZ_PORT=$hz_port" > hazelcast.env
 
 sed -i "s/EB_ENV_NAME/$(/opt/elasticbeanstalk/bin/get-config container -k environment_name)/g" docker-compose.yml
 
-# Set up memory
+#################
+# Set up memory #
+#################
 
 # Use 81% of available memory for heap
 heap_ratio="0.81"
@@ -33,11 +39,21 @@ huge_page_ratio="0.95"
 # https://docs.redhat.com/en/documentation/red_hat_data_grid/7.1/html/performance_tuning_guide/configure_page_memory
 huge_page_size=$(echo "2 * 1024 * 1024" | bc)
 
+# Reserve at least 1 GB
+reserved_mem=$(echo "1024^3" | bc)
+
 avail_mem=$(awk '/MemTotal/ {print $2 * 1024}' /proc/meminfo)
 
 heap=$(echo "scale=0; $avail_mem * $heap_ratio / 1" | bc)
 
-page_count=$(echo "scale=0; $avail_mem * $huge_page_ratio / $huge_page_size" | bc)
+# We'll take the min of mem_a or mem_b for huge pages, so that we reserve
+# at least 1gb of memory
+mem_a=$(echo "$avail_mem * $huge_page_ratio" | bc)
+mem_b=$(echo "$avail_mem - $reserved_mem" | bc);
+
+huge_page_mem=$(echo "define min(a,b){ if(a>b){return(b)}; return(a) }; min($mem_a, $mem_b)" | bc -l)
+
+page_count=$(echo "scale=0; $huge_page_mem / $huge_page_size" | bc)
 
 echo "$page_count" > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
 
