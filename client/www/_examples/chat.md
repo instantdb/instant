3196202b7b034f94440ee558649e118da6f8ff59
Joe Averbukh <joeaverbukh@gmail.com>
2025-10-23 16:32:06 -0700

    Add app examples page

---
+++ client/www/_examples/chat.md
@@ -0,0 +1,50 @@
+![](/img/showcase/chat_preview.png 'IRC style chat with guest login!')
+
+# Quickstart
+
+Clone the repo and install dependencies:
+
+```bash
+# Clone repo
+git clone https://github.com/instantdb/instant-examples
+
+# Navigate into the chat example
+cd instant-examples/chat
+
+# Install dependencies
+pnpm i
+```
+
+If you haven't already, be sure to log into the Instant CLI
+
+```bash
+pnpx instant-cli login
+```
+
+Create a new app in the [Instant Dashboard](https://www.instantdb.com/dash)
+and copy the `INSTANT_APP_ID` and `INSTANT_API_KEY` into your `.env` file.
+
+```bash
+# Copy .env.example to .env and update the variables
+# by creating a new app at https://www.instantdb.com/dash
+cp .env.example .env
+```
+
+We've provided a schema in `instant.schema.ts` that you can push to your app via
+the CLI:
+
+```bash
+pnpx instant-cli push
+```
+
+Run the seed script to populate the database with some initial data:
+
+```bash
+pnpm run seed
+```
+
+Finally, run the development server:
+
+```bash
+pnpm run dev
+```
---
+++ client/www/lib/examples/data.ts
@@ -0,0 +1,81 @@
+export interface AppMetadata {
+  slug: string;
+  title: string;
+  linesOfCode: string;
+  tags: string[];
+  description: string;
+  shortDescription: string;
+  screenshot: string;
+  githubUrl: string;
+  youtubeVideoId?: string;
+}
+
+const todoDescription = `
+A simple todo app that allows you to create, read, update, and delete
+todo items. Additionally, it tracks and displays the number of active viewers
+currently looking at the todo list.
+
+As a fun bonus, everything is real-time and works offline! You don't need to
+write any special code to get these features; they are built-in with Instant.
+`.trim();
+
+const blogDescription = `
+A microblog app that allows users to create and like posts.
+
+By default anyone can view posts, but only authenticated users can create posts and like
+them. User profiles with unique usernames are also supported.
+
+We also demonstrate how to use the Admin SDK to both bootstrap and reset the
+database. jhis can be super useful for testing and development!
+`.trim();
+
+const chatDescription = `
+A chat app showing who's online with support for mulitple channels.
+
+In this app we demonstrate how to use guest authentication to allow users to join
+without fully signing-up. Signed in users show up in the online list and can
+send messages to the chat.
+
+Different channels are supported and presences and messages are scoped to each channel.
+`.trim();
+
+export const appMetas: AppMetadata[] = [
+  {
+    slug: 'todos',
+    title: 'Todo App',
+    linesOfCode: `~100`,
+    tags: [
+      'Basic schema',
+      'Basic Queries',
+      'Basic Transactions',
+      'Basic Presence',
+    ],
+    description: todoDescription,
+    shortDescription: 'See the basics of InstantDB in action',
+    screenshot: '/img/showcase/todos_preview.png',
+    githubUrl: 'https://github.com/instantdb/instant-examples/tree/main/todos',
+    youtubeVideoId: '827EPRQ0ww0',
+  },
+  {
+    slug: 'microblog',
+    title: 'Microblog',
+    linesOfCode: `~400`,
+    tags: ['Auth', 'Profiles', 'Schema Links', 'Seed Data'],
+    description: blogDescription,
+    shortDescription:
+      'Learn how to model relationships and use authentication with InstantDB',
+    screenshot: '/img/showcase/blog_preview.png',
+    githubUrl:
+      'https://github.com/instantdb/instant-examples/tree/main/microblog',
+  },
+  {
+    slug: 'chat',
+    title: 'Chat',
+    linesOfCode: '~300',
+    tags: ['Guest Auth', 'Presence', 'Seed Data'],
+    shortDescription: 'Build a real-time chat app with presence',
+    description: chatDescription,
+    screenshot: 'img/showcase/chat_preview.png',
+    githubUrl: 'https://github.com/instantdb/instant-examples/tree/main/chat',
+  },
+];
