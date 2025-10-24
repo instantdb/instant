f0d00cb33a5db04e0fcfccfc696d1d446307f8d4
Daniel Woelfel <dwwoelfel@gmail.com>
2025-10-24 15:32:39 -0700

    Don't delete pending mutations that are in progress (#1884)

--- client/packages/version/src/version.ts
+++ client/packages/version/src/version.ts
@@ -1,7 +1,6 @@
 // This is the shared version for all of the js packages
 // Update the version here and merge your code to main to
 // publish a new version of all of the packages to npm.
-
-const version = 'v0.22.33';
+const version = 'v0.22.34';
 
 export { version };
