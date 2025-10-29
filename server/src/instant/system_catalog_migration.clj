664560243bfc8759708c50685d9e3bab21a9b794
stopachka <stepan.p@gmail.com>
2025-10-29 11:27:01 -0700

    roar

--- server/src/instant/system_catalog_migration.clj
+++ server/src/instant/system_catalog_migration.clj
@@ -6,7 +6,8 @@
    [instant.jdbc.sql :as sql]
    [instant.system-catalog :as system-catalog]
    [instant.util.tracer :as tracer]
-   [lambdaisland.deep-diff2 :as ddiff]))
+   [lambdaisland.deep-diff2 :as ddiff]
+   [next.jdbc]))
 
 (defn missing-attrs [existing-attrs]
   (filter (fn [attr]
@@ -38,6 +39,7 @@
                :label (attr-model/fwd-label attr)}))
           db-attrs)))
 
+(def conn (aurora/conn-pool :write))
 (defn ensure-attrs-on-system-catalog-app
   ([]
    (ensure-attrs-on-system-catalog-app system-catalog/system-catalog-app-id))
@@ -70,21 +72,25 @@
          (tracer/record-info! {:name "system-catalog/extra-attrs"
                                :attributes {:extras extras}}))
        (when (seq json-ids)
-         (sql/execute!
-          (aurora/conn-pool :write)
-          (hsql/format {:update :attrs
-                        :where [:in :id json-ids]
-                        :set {:inferred-types [:cast
-                                               (attr-model/binary-inferred-types
-                                                #{:json})
-                                               [:bit :32]]}})))
+         (next.jdbc/with-transaction [conn (aurora/conn-pool :write)]
+           (sql/execute! conn ["set local instant.allow_system_catalog_app_attr_update = true"])
+           (sql/execute!
+            conn
+            (hsql/format {:update :attrs
+                          :where [:in :id json-ids]
+                          :set {:inferred-types [:cast
+                                                 (attr-model/binary-inferred-types
+                                                  #{:json})
+                                                 [:bit :32]]}}))))
        (when (seq string-ids)
-         (sql/execute!
-          (aurora/conn-pool :write)
-          (hsql/format {:update :attrs
-                        :where [:in :id string-ids]
-                        :set {:inferred-types [:cast
-                                               (attr-model/binary-inferred-types
-                                                #{:string})
-                                               [:bit :32]]}})))
+         (next.jdbc/with-transaction [conn (aurora/conn-pool :write)]
+           (sql/execute! conn ["set local instant.allow_system_catalog_app_attr_update = true"])
+           (sql/execute!
+            conn
+            (hsql/format {:update :attrs
+                          :where [:in :id string-ids]
+                          :set {:inferred-types [:cast
+                                                 (attr-model/binary-inferred-types
+                                                  #{:string})
+                                                 [:bit :32]]}}))))
        (tracer/add-data! {:attributes {:created-count (count ids)}})))))
