08194b09d7ff730d34c482685d462e842a4e19dd
Stepan Parunashvili <stepan.p@gmail.com>
2025-10-29 17:25:55 -0700

    Update server/resources/migrations/92_editable_attrs.up.sql
    Co-authored-by: Daniel Woelfel <dwwoelfel@gmail.com>

--- server/resources/migrations/92_editable_attrs.up.sql
+++ server/resources/migrations/92_editable_attrs.up.sql
@@ -2,11 +2,13 @@ create or replace function prevent_required_on_reserved_attrs()
 returns trigger as $$
 begin
   if new.is_required
-    and starts_with(new.etype, '$')
-    and new.app_id <> 'a1111111-1111-1111-1111-111111111ca7' then
-        raise exception 'required_set_on_reserved_attrs';
-    end if;
-    return new;
+     and starts_with(new.etype, '$')
+     and new.app_id <> 'a1111111-1111-1111-1111-111111111ca7'
+  then
+    raise exception 'required_set_on_reserved_attrs';
+  end if;
+  
+  return new;
 end;
 $$ language plpgsql;
 
