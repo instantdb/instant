28306acce20ecf18d82329d77e5db9993c55593a
Drew Harris <drew@drewh.net>
2025-11-05 10:24:19 -0800

    useAuth returns the $users type from schema (#1908) [bump version to v0.22.46]

--- client/packages/core/src/clientTypes.ts
+++ client/packages/core/src/clientTypes.ts
@@ -1,3 +1,6 @@
+import { Expand } from './queryTypes.ts';
+import { InstantSchemaDef, ResolveAttrs } from './schemaTypes.ts';
+
 export type User = {
   id: string;
   refresh_token: string;
@@ -7,14 +10,29 @@ export type User = {
   isGuest: boolean;
 };
 
-export type AuthResult =
-  | { user: User | undefined; error: undefined }
+export type UserWithSchema<
+  S extends InstantSchemaDef<any, any, any>,
+  UseDates extends boolean,
+> = Expand<ResolveAttrs<S['entities'], '$users', UseDates> & User>;
+
+export type AuthResult<
+  S extends InstantSchemaDef<any, any, any>,
+  UseDates extends boolean,
+> =
+  | { user: UserWithSchema<S, UseDates> | undefined; error: undefined }
   | { user: undefined; error: { message: string } };
 
-export type AuthState =
+export type AuthState<
+  S extends InstantSchemaDef<any, any, any>,
+  UseDates extends boolean = false,
+> =
   | { isLoading: true; error: undefined; user: undefined }
   | { isLoading: false; error: { message: string }; user: undefined }
-  | { isLoading: false; error: undefined; user: User | null };
+  | {
+      isLoading: false;
+      error: undefined;
+      user: UserWithSchema<S, UseDates> | null;
+    };
 
 export type ConnectionStatus =
   | 'connecting'
