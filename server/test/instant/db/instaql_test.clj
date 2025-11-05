68609cfe5fdd3975735d50e53aa58c5c22a1bac4
stopachka <stepan.p@gmail.com>
2025-11-05 11:30:26 -0800

    field rules on links work

--- server/test/instant/db/instaql_test.clj
+++ server/test/instant/db/instaql_test.clj
@@ -4913,5 +4913,25 @@
         (is (= #{"joe@instantdb.com"} all-emails))
         (is (= #{"alex"} all-handles))))))
 
+(deftest field-rules-can-filter-in-where-clauses
+  (with-zeneca-app
+    (fn [app _r]
+      (let [make-ctx (fn []
+                       (let [attrs (attr-model/get-by-app-id (:id app))]
+                         {:db {:conn-pool (aurora/conn-pool :read)}
+                          :app-id (:id app)
+                          :attrs attrs}))
+
+            _ (rule-model/put! (aurora/conn-pool :write)
+                               {:app-id (:id app)
+                                :code {:bookshelves {:allow {:view "true"}}
+                                       :users {:allow {:view "true"}
+                                               :fields {:handle "data.handle == 'alex'"}}}})]
+
+        (is (= 2
+               (count (:bookshelves (pretty-perm-q (make-ctx) {:bookshelves {:$ {:where {"users.handle" "alex"}}}})))))
+        (is (= 0
+               (count (:bookshelves (pretty-perm-q (make-ctx) {:bookshelves {:$ {:where {"users.handle" "joe"}}}})))))))))
+
 (comment
   (test/run-tests *ns*))
