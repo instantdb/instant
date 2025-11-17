cfb757178f99ce4888cd739a9e4336ebc488603e
Daniel Woelfel <dwwoelfel@gmail.com>
2025-11-17 15:50:17 -0800

    missing files

---
+++ client/packages/core/src/InMemoryStorage.ts
@@ -0,0 +1,32 @@
+import type { Storage } from './utils/PersistedObject';
+
+export default class InMemoryStorage implements Storage {
+  private dbName: string;
+  private store: Map<string, any>;
+  constructor(dbName) {
+    this.dbName = dbName;
+    this.store = new Map();
+  }
+
+  async getItem(k) {
+    return this.store.get(k) ?? null;
+  }
+
+  async setItem(k, v) {
+    this.store.set(k, v);
+  }
+
+  async getAllKeys(): Promise<string[]> {
+    return [...this.store.keys()];
+  }
+
+  async multiSet(keyValuePairs: Array<[string, any]>): Promise<void> {
+    for (const [k, v] of keyValuePairs) {
+      this.setItem(k, v);
+    }
+  }
+
+  async removeItem(key: string): Promise<void> {
+    this.store.delete(key);
+  }
+}
---
+++ client/packages/react-native/src/Storage.native.ts
@@ -0,0 +1,37 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import type { StorageInterface } from '@instantdb/core';
+
+export default class Storage implements StorageInterface {
+  private dbName: string;
+  constructor(dbName) {
+    this.dbName = dbName;
+  }
+
+  private makeKey(k: string): string {
+    return `${this.dbName}_${k}`;
+  }
+
+  async getItem(k) {
+    const res = await AsyncStorage.getItem(this.makeKey(k));
+    return JSON.parse(res);
+  }
+
+  async setItem(k, v) {
+    await AsyncStorage.setItem(this.makeKey(k), JSON.stringify(v));
+  }
+
+  async removeItem(k: string): Promise<void> {
+    await AsyncStorage.removeItem(this.makeKey(k));
+  }
+
+  async getAllKeys(): Promise<string[]> {
+    const keys = await AsyncStorage.getAllKeys();
+    return keys.filter((k) => k.startsWith(`${this.dbName}_`));
+  }
+
+  async multiSet(keyValuePairs: Array<[string, any]>): Promise<void> {
+    await AsyncStorage.multiSet(
+      keyValuePairs.map(([k, v]) => [this.makeKey(k), v]),
+    );
+  }
+}
