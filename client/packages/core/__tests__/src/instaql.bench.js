259e5e5f9821b6a632ddd5364ae8e357ce221e95
Drew Harris <drew@drewh.net>
2025-11-20 14:22:29 -0800

    entity caching in instaql

--- client/packages/core/__tests__/src/instaql.bench.js
+++ client/packages/core/__tests__/src/instaql.bench.js
@@ -1,9 +1,22 @@
-import { bench } from 'vitest';
+import { bench, describe } from 'vitest';
 
 import zenecaAttrs from './data/zeneca/attrs.json';
+import * as instaml from '../../src/instaml';
 import zenecaTriples from './data/zeneca/triples.json';
-import { createStore } from '../../src/store';
+import { createStore, transact, getAllTriples } from '../../src/store';
 import query from '../../src/instaql';
+import { id, txInit } from '../../src';
+
+function generateRandomString(length) {
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 
 const zenecaIdToAttr = zenecaAttrs.reduce((res, x) => {
   res[x.id] = x;
@@ -26,4 +39,30 @@ bench('big query', () => {
       },
     },
   );
+});
+
+describe('entity caching', () => {
+  const emptyStore = createStore({}, []);
+  const tx = txInit();
+
+  const chunks = [];
+  for (let i = 0; i < 100000; i++) {
+    chunks.push(
+      tx.hi[id()].create({
+        name: generateRandomString(10),
+        age: Math.floor(Math.random() * 100),
+        bio: generateRandomString(50),
+      }),
+    );
+  }
+
+  const txSteps = instaml.transform({ attrs: store.attrs }, chunks);
+
+  const newStore = transact(emptyStore, txSteps);
+
+  console.log(newStore.attrs);
+
+  const attrs = newStore.attrs;
+  const triples = getAllTriples(newStore);
+  console.log(triples.length);
 });
