da05796d2cac9bf9c1a90f53065c330d42dcb28d
Daniel Woelfel <dwwoelfel@gmail.com>
2025-11-20 14:06:30 -0800

    fix race in lookups

--- server/test/instant/db/transaction_test.clj
+++ server/test/instant/db/transaction_test.clj
@@ -5570,5 +5570,25 @@
             (testing "connection did not deadlock"
               (is @tx))))))))
 
+
+;; Test that we don't get a conflict if a bunch of lookup inserts are happening simultaneously
+(deftest multiple-lookups-work
+  (with-zeneca-app
+    (fn [{app-id :id
+          make-ctx :make-ctx}
+         r]
+      (let [handle-aid (resolvers/->uuid r :users/handle)
+            lookup [handle-aid (random-uuid)]
+            tx-data [[:add-triple lookup (resolvers/->uuid r :users/id) lookup]
+                     [:add-triple lookup (resolvers/->uuid r :users/bookshelves) (random-uuid)]]
+            attrs (attr-model/get-by-app-id app-id)
+            conn (aurora/conn-pool :write)
+            txes (mapv (fn [_]
+                         (future (permissioned-tx/transact! (make-ctx) tx-data)))
+                       (range 20))]
+        (mapv (fn [tx]
+                (is @tx))
+              txes)))))
+
 (comment
   (test/run-tests *ns*))
