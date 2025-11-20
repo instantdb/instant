435eef7a3a5cee69036d14ba19900edd92c31d46
Joe Averbukh <joeaverbukh@gmail.com>
2025-11-19 17:25:39 -0800

    Remove llm example apps

--- client/www/lib/intern/llm-example/lib/db.ts
+++
@@ -1,7 +0,0 @@
-import { init } from '@instantdb/react';
-import schema from '../instant.schema';
-
-const APP_ID = process.env.NEXT_PUBLIC_LLM_EXAMPLE_APP_ID!;
-const db = init({ appId: APP_ID, schema });
-
-export default db;
