52d6657ed225e7dfd7e387ba3f51b345d5e138e9
Daniel Woelfel <dwwoelfel@gmail.com>
2025-11-25 15:13:18 -0800

    missed a file

---
+++ server/src/instant/admin/transact_queue.clj
@@ -0,0 +1,53 @@
+(ns instant.admin.transact-queue
+  (:require
+   [instant.config :as config]
+   [instant.db.permissioned-transaction :as permissioned-tx]
+   [instant.grouped-queue :as grouped-queue]
+   [instant.util.delay :as delay]
+   [instant.util.exception :as ex]))
+
+(def num-receive-workers (* (if config/fewer-vfutures?
+                              20
+                              100)
+                            (delay/cpu-count)))
+
+(declare tx-q)
+
+(defn put!
+  ([item]
+   (grouped-queue/put! tx-q item))
+  ([q item]
+   (grouped-queue/put! q item)))
+
+(defn attr-key [tx-steps]
+  (when (= :add-triple (ffirst tx-steps))
+    (-> tx-steps first (nth 2))))
+
+(defn group-key [{:keys [app-id tx-steps]}]
+  [app-id (or (attr-key tx-steps)
+              (random-uuid))])
+
+(defn combine [_a _b]
+  nil)
+
+(defn process [_group-key {:keys [ctx tx-steps response-promise open?]}]
+  (try
+    (when-not (tool/inspect (open?))
+      (ex/throw-connection-closed!))
+    (deliver response-promise {:ok (permissioned-tx/transact! ctx tx-steps)})
+    (catch Throwable t
+      (deliver response-promise {:error t}))))
+
+(defn start []
+  (.bindRoot #'tx-q
+             (grouped-queue/start
+               {:group-key-fn #'group-key
+                :combine-fn   #'combine
+                :process-fn   #'process
+                :max-workers  num-receive-workers
+                :metrics-path "instant.admin.transact-queue"})))
+
+(defn stop []
+  (when (bound? #'tx-q)
+    (grouped-queue/stop tx-q)
+    (.unbindRoot #'tx-q)))
