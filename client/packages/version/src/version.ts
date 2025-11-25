dcf6e25fa75981d7930c01340eae0b547fe54086
Joe Averbukh <joeaverbukh@gmail.com>
2025-11-24 16:11:56 -0800

    bump

--- client/packages/version/src/version.ts
+++ client/packages/version/src/version.ts
@@ -2,6 +2,6 @@
 // Update the version here and merge your code to main to
 // publish a new version of all of the packages to npm.
 
-const version = 'v0.22.70';
+const version = 'v0.22.71';
 
 export { version };
