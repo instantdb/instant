6018a6a2595174c6e3cafb4bb33c81f83783eae0
Daniel Woelfel <dwwoelfel@gmail.com>
2025-11-30 17:15:11 -0800

    fix tests

--- server/test/instant/reactive/store_test.clj
+++ server/test/instant/reactive/store_test.clj
@@ -13,19 +13,19 @@
   (is (true?
         (rs/match-topic?
           '[:eav _ _ _]
-          '[:eav #{1} #{2} #{3}])))
+          (rs/squish-topics '[[:eav #{1} #{2} #{3}]]))))
   (is (true?
         (rs/match-topic?
           '[:eav #{1 2} _ _]
-          '[:eav #{1} #{2} #{3}])))
+          (rs/squish-topics '[[:eav #{1} #{2} #{3}]]))))
   (is (false?
         (rs/match-topic?
           '[:ea #{1 2} _ _]
-          '[:eav #{1} #{2} #{3}])))
+          (rs/squish-topics '[[:eav #{1} #{2} #{3}]]))))
   (is (false?
         (rs/match-topic?
           '[:eav #{3} _ _]
-          '[:eav #{1} #{2} #{3}]))))
+          (rs/squish-topics '[[:eav #{1} #{2} #{3}]])))))
 
 (deftest swap-datalog-cache!
   (let [store  (rs/init)
