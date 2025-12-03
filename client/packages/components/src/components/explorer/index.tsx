4d35bbe9c6f4ce7c813ca31f6422b3ed9e330ab5
Drew Harris <drew@drewh.net>
2025-12-03 09:51:55 -0800

    pre move everything over

--- client/packages/components/src/components/explorer/index.tsx
+++ client/packages/components/src/components/explorer/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleMe } from './StyleMe.js';
+import { StyleMe } from '../StyleMe.js';
 interface ExplorerProps {
   appId: string;
   adminToken: string;
@@ -10,7 +10,7 @@ interface ExplorerProps {
 export const Explorer = ({ appId, adminToken }: ExplorerProps) => {
   return (
     <StyleMe>
-      <div className="tw-preflight bg-[#ffcabb] p-2">
+      <div className="tw-preflight bg-red-400 p-2">
         {adminToken}
         <p>This is the explorer</p>
       </div>
