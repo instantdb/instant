8915926c0678502b650a48ae341983689961fd77
Daniel Woelfel <dwwoelfel@gmail.com>
2025-12-05 09:35:37 -0800

    missing transaction changes

--- server/src/instant/db/model/transaction.clj
+++ server/src/instant/db/model/transaction.clj
@@ -1,5 +1,6 @@
 (ns instant.db.model.transaction
   (:require
+   [instant.flags :as flags]
    [instant.jdbc.sql :as sql]
    [instant.jdbc.aurora :as aurora]))
 
@@ -8,5 +9,14 @@
   ([conn {:keys [app-id]}]
    (sql/execute-one! ::create!
                      conn
-                     ["INSERT INTO transactions (app_id) VALUES (?::uuid)"
-                      app-id])))
+                     (if (flags/enable-wal-entity-log? app-id)
+                       (tool/inspect ["with setting as materialized (
+                           select set_config('instant.wal_msg_app_id', ?::text, true) as config
+                         ),
+                         tx as (
+                           insert into transactions (app_id) values (?::uuid) returning *
+                         )
+                         select tx.*, (select config from setting) as _ from tx"
+                                      (str app-id)
+                                      app-id])
+                       (tool/inspect ["insert into transactions (app_id) values (?::uuid)" app-id])))))
