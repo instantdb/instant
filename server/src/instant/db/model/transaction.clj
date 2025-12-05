e5d67c15ff392f8b50b9115c292c6ed6eff12088
Daniel Woelfel <dwwoelfel@gmail.com>
2025-12-05 09:36:12 -0800

    [staging] fix stray checkin

--- server/src/instant/db/model/transaction.clj
+++ server/src/instant/db/model/transaction.clj
@@ -1,23 +1,12 @@
 (ns instant.db.model.transaction
   (:require
-   [instant.flags :as flags]
    [instant.jdbc.sql :as sql]
    [instant.jdbc.aurora :as aurora]))
 
 (defn create!
   ([params] (create! (aurora/conn-pool :write) params))
   ([conn {:keys [app-id]}]
-   (tool/def-locals)
    (sql/execute-one! ::create!
                      conn
-                     (if (flags/enable-wal-entity-log? app-id)
-                       (tool/inspect ["with setting as materialized (
-                           select set_config('instant.wal_msg_app_id', ?::text, true) as config
-                         ),
-                         tx as (
-                           insert into transactions (app_id) values (?::uuid) returning *
-                         )
-                         select tx.*, (select config from setting) as _ from tx"
-                                      (str app-id)
-                                      app-id])
-                       (tool/inspect ["insert into transactions (app_id) values (?::uuid)" app-id])))))
+                     ["INSERT INTO transactions (app_id) VALUES (?::uuid)"
+                      app-id])))
