f91656e9b8e8a8932eedd70aaa1f61daaef9266a
Stepan Parunashvili <stepan.p@gmail.com>
2025-12-09 16:54:22 -0800

    [easy] include normalized forms in refresh-spam (#2083)

--- server/src/instant/reactive/session.clj
+++ server/src/instant/reactive/session.clj
@@ -38,6 +38,7 @@
    [instant.util.json :refer [<-json]]
    [instant.util.semver :as semver]
    [instant.util.e2e-tracer :as e2e-tracer]
+   [instant.util.instaql :as instaql-util]
    [instant.util.tracer :as tracer]
    [instant.util.uuid :as uuid-util]
    [lambdaisland.uri :as uri])
@@ -425,7 +426,8 @@
 
 (defn- recompute-instaql-query!
   [{:keys [store current-user app-id sess-id attrs table-info admin?]}
-   {:keys [instaql-query/query instaql-query/return-type instaql-query/inference?]}]
+   {:keys [instaql-query/query instaql-query/return-type instaql-query/inference?
+           instaql-query/forms-hash]}]
   (let [ctx {:db {:conn-pool (aurora/conn-pool (db-read-level app-id))}
              :session-id sess-id
              :app-id app-id
@@ -439,6 +441,7 @@
         (rq/instaql-query-reactive! store ctx query return-type inference?)
         end-ms (System/currentTimeMillis)]
     {:instaql-query query
+     :instaql-query-hash forms-hash
      :instaql-result instaql-result
      :result-meta result-meta
      :result-changed? result-changed?
@@ -482,10 +485,12 @@
         can-skip-attrs? (supports-skip-attrs? features)
         attrs-hash      (hash attrs)
         attrs-changed?  (not= prev-attrs-hash attrs-hash)]
-    (doseq [{:keys [instaql-query duration-ms instaql-topic?]} spam]
+    (doseq [{:keys [instaql-query instaql-query-hash duration-ms instaql-topic?]} spam]
       (tracer/record-info! {:name "handle-refresh/spam"
                             :attributes {:app-id app-id
                                          :instaql-query instaql-query
+                                         :instaql-query-hash instaql-query-hash
+                                         :instaql-query-normalized (instaql-util/normalized-forms instaql-query)
                                          :duration-ms duration-ms
                                          :instaql-topic? instaql-topic?}}))
 
