74bf76f02e219d8c4be4f0429f4d4c3362f384be
Drew Harris <drew@drewh.net>
2025-12-18 11:11:33 -0800

    fix get user on server

--- client/packages/react/src/next-ssr/getUserOnServer.ts
+++ client/packages/react/src/next-ssr/getUserOnServer.ts
@@ -1,8 +1,8 @@
 import { cookies } from 'next/headers.js';
 
-export const getUserOnServer = async () => {
+export const getUserOnServer = async (appId: string) => {
   const cookieStore = await cookies();
-  const userJSON = cookieStore.get('instant_user');
+  const userJSON = cookieStore.get('instant_user_' + appId);
   const user = userJSON ? JSON.parse(userJSON.value) : null;
   return user;
 };
--- client/sandbox/react-nextjs/app/play/ssr/layout.tsx
+++ client/sandbox/react-nextjs/app/play/ssr/layout.tsx
@@ -1,13 +1,9 @@
-import { cookies } from 'next/headers';
-import { appId } from './db';
-import schema from './instant.schema';
 import { InstantProvider } from './InstantProvider';
+import { getUserOnServer } from '@instantdb/react/nextjs';
 
 // (server page)
 export default async function ({ children }: { children: React.ReactNode }) {
-  const cookieStore = await cookies();
-  const userJSON = cookieStore.get('instant_user');
-  const user = userJSON ? JSON.parse(userJSON.value) : null;
+  const user = await getUserOnServer(process.env.NEXT_PUBLIC_INSTANT_APP_ID!);
 
   if (!process.env.NEXT_PUBLIC_INSTANT_APP_ID) {
     return <div>No app id found</div>;
