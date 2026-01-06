16595720cf3ddf78842955281ee8355d93764b7e
Drew Harris <drew@drewh.net>
2026-01-06 13:12:07 -0800

    effect rewrite of cli
     - finish global opts
     - info command
     - move around init
     - more stuff
     - pull all + push perms done
     - a lot done + claim
     - start of push
     - wow?
     - clean up old version + info
     - parity
     - opts from command
     - remove todo
     - add back old pull
     - formatting
     - finish init
     - schema read file error
     - swap to coerce login
     - fix ts error
     - normaler error messages
     - fix type
     - fix logout
    
    fix coerce
    
    same error format
    
    feedback
    
    enable silent logging
    
    more logging fixes
    
    low hanging fruit
    
    explorer command
    
    track app imports
    
    add bun as potential env
    
    add solid package
    
    format for init without files
    
    allow admin token as auth
    
    fix ci tests - use new cli
    
    experimental type preservation
    
    query command
    
    instant.config.ts support
    
    fix a lot

---
+++ client/packages/cli/src/new/commands/login.ts
@@ -0,0 +1,75 @@
+import chalk from 'chalk';
+import { Effect } from 'effect';
+import openInBrowser from 'open';
+import { OptsFromCommand, loginDef } from '../index.js';
+import { getDashUrl } from '../lib/http.js';
+import {
+  getLoginTicketAndSecret,
+  saveConfigAuthToken,
+  waitForAuthToken,
+} from '../lib/login.js';
+import { promptOk } from '../lib/ui.js';
+
+const isHeadLessEnvironment = (opts: OptsFromCommand<typeof loginDef>) => {
+  const noBrowserMode = Boolean(
+    process.env.INSTANT_CLI_NO_BROWSER || process.env.CI || opts?.headless,
+  );
+
+  // Check for common headless environment indicators
+  return (
+    noBrowserMode ||
+    process.env.TERM === 'dumb' ||
+    process.env.SSH_CONNECTION !== undefined ||
+    process.env.SSH_CLIENT !== undefined ||
+    (!process.env.DISPLAY && process.platform === 'linux') ||
+    process.env.WSL_DISTRO_NAME !== undefined
+  );
+};
+
+export const loginCommand = Effect.fn(function* (
+  opts: OptsFromCommand<typeof loginDef>,
+) {
+  yield* Effect.log("Let's log you in!");
+
+  const loginInfo = yield* getLoginTicketAndSecret;
+  const { secret, ticket } = loginInfo;
+  const dashOrigin = yield* getDashUrl;
+  yield* Effect.log();
+  // TODO: flip these so rejecting the prompt prints url
+  if (isHeadLessEnvironment(opts)) {
+    yield* Effect.log(
+      `Open this URL in a browser to log in:\n ${dashOrigin}/dash?ticket=${ticket}\n`,
+    );
+  } else {
+    const ok = yield* promptOk(
+      {
+        promptText:
+          'This will open instantdb.com in your browser, OK to proceed?',
+      },
+      true,
+    );
+    if (!ok) {
+      process.exit(1);
+    }
+    yield* Effect.tryPromise(() =>
+      openInBrowser(`${dashOrigin}/dash?ticket=${ticket}`),
+    );
+  }
+
+  yield* Effect.log('Waiting for authentication...');
+
+  const result = yield* waitForAuthToken(secret);
+  const { token, email } = result;
+  if (opts.print) {
+    yield* Effect.log(
+      chalk.red('[Do not share] Your Instant auth token:', token),
+    );
+  } else {
+    yield* saveConfigAuthToken(token);
+    yield* Effect.log(chalk.green(`Successfully logged in as ${email}!`));
+  }
+  return {
+    authToken: token,
+    source: 'file' as const,
+  };
+});
---
+++ client/packages/cli/src/new/logging.ts
@@ -0,0 +1,32 @@
+import chalk from 'chalk';
+import { HashMap, Logger, Match, Option } from 'effect';
+
+export function warn(firstArg, ...rest) {
+  console.warn(chalk.yellow('[warning]') + ' ' + firstArg, ...rest);
+}
+
+export function error(firstArg, ...rest) {
+  console.error(chalk.red('[error]') + ' ' + firstArg, ...rest);
+}
+
+const simpleLogger = Logger.make(({ logLevel, message, annotations }) => {
+  const isSilent = HashMap.get(annotations, 'silent').pipe(Option.getOrNull);
+  if (isSilent) return;
+  const formattedMessage = Array.isArray(message) ? message.join(' ') : message;
+  Match.value(logLevel).pipe(
+    Match.tag('Info', () => console.log(formattedMessage)),
+    Match.tag('Warning', () => console.warn(formattedMessage)),
+    Match.tag('Error', () =>
+      console.error(chalk.red('[error]') + ' ' + formattedMessage),
+    ),
+    Match.tag('Debug', () => console.debug(formattedMessage)),
+    Match.tag('Fatal', () =>
+      console.error(chalk.red('[error]') + ' ' + formattedMessage),
+    ),
+  );
+});
+
+export const SimpleLogLayer = Logger.replace(
+  Logger.defaultLogger,
+  simpleLogger,
+);
