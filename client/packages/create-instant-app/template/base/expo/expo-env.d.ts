51d706859609d73d49e97570227f50718a6f04e1
Drew Harris <drew@drewh.net>
2026-01-22 14:09:12 -0800

    remove old templates

--- client/packages/create-instant-app/template/base/expo/expo-env.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="expo/types" />
-
-// NOTE: This file should not be edited and should be in your git ignore
--- client/packages/create-instant-app/template/base/next-js-app-dir/src/lib/db.ts
+++
@@ -1,8 +0,0 @@
-import { init } from "@instantdb/react";
-import schema from "../instant.schema";
-
-export const db = init({
-  appId: process.env.NEXT_PUBLIC_INSTANT_APP_ID!,
-  schema,
-  useDateObjects: true,
-});
