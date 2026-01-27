da9d185a040d19659801fa3beb01478c68b1f219
Drew Harris <drew@drewh.net>
2026-01-27 10:11:25 -0800

    testing the new auth method

---
+++ client/sandbox/react-nextjs/app/api/authcheck/route.ts
@@ -0,0 +1,15 @@
+import { init } from '@instantdb/admin';
+import schema from '../../play/ssr/instant.schema';
+
+export const appId = process.env.NEXT_PUBLIC_INSTANT_APP_ID;
+
+export const db = init({
+  appId: appId!,
+  apiURI: 'http://localhost:8888',
+  schema,
+  useDateObjects: true,
+});
+
+export const GET = async (req: Request) => {
+  return new Response(JSON.stringify(await db.auth.getUserFromRequest(req)));
+};
