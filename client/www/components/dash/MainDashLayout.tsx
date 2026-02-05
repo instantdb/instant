48b06ba3d7b4678e19a51c1f222e4275bb1740cc
Drew Harris <drew@drewh.net>
2026-02-04 16:00:21 -0800

    fix dashboard overflow

--- client/www/components/dash/MainDashLayout.tsx
+++ client/www/components/dash/MainDashLayout.tsx
@@ -175,7 +175,7 @@ export const MainDashLayout: React.FC<{
       >
         <div
           className={cn(
-            'flex min-h-full w-full flex-col',
+            'flex h-[100dvh] w-full flex-col',
             darkMode ? 'dark' : '',
           )}
         >
