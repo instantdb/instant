042fce6a74ef675674d54794c1faff7d1b249777
Joe Averbukh <joeaverbukh@gmail.com>
2026-02-10 10:41:48 -0800

    lint

--- client/sandbox/react-nextjs/config.ts
+++ client/sandbox/react-nextjs/config.ts
@@ -6,7 +6,9 @@ const isProd =
 const localPort = process.env.NEXT_PUBLIC_LOCAL_SERVER_PORT || '8888';
 
 const config = {
-  apiURI: isProd ? 'https://api.instantdb.com' : `http://localhost:${localPort}`,
+  apiURI: isProd
+    ? 'https://api.instantdb.com'
+    : `http://localhost:${localPort}`,
   websocketURI: isProd
     ? 'wss://api.instantdb.com/runtime/session'
     : `ws://localhost:${localPort}/runtime/session`,
