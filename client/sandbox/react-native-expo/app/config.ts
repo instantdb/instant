cd6b61d31864b19ccb2cb4906ab93f39e78fdf54
Joe Averbukh <joeaverbukh@gmail.com>
2026-02-10 10:39:10 -0800

    Add additional config

--- client/sandbox/react-native-expo/app/config.ts
+++ client/sandbox/react-native-expo/app/config.ts
@@ -1,7 +1,9 @@
+const localPort = process.env.EXPO_PUBLIC_LOCAL_SERVER_PORT || '8888';
+
 const config = {
   appId: process.env.EXPO_PUBLIC_INSTANT_APP_ID,
-  apiURI: 'http://localhost:8888',
-  websocketURI: 'ws://localhost:8888/runtime/session',
+  apiURI: `http://localhost:${localPort}`,
+  websocketURI: `ws://localhost:${localPort}/runtime/session`,
   verbose: true,
 };
 
--- client/sandbox/react-nextjs/config.ts
+++ client/sandbox/react-nextjs/config.ts
@@ -3,11 +3,13 @@ const isProd =
     ? Boolean(localStorage.getItem('prodBackend'))
     : false;
 
+const localPort = process.env.NEXT_PUBLIC_LOCAL_SERVER_PORT || '8888';
+
 const config = {
-  apiURI: isProd ? 'https://api.instantdb.com' : 'http://localhost:8888',
+  apiURI: isProd ? 'https://api.instantdb.com' : `http://localhost:${localPort}`,
   websocketURI: isProd
     ? 'wss://api.instantdb.com/runtime/session'
-    : 'ws://localhost:8888/runtime/session',
+    : `ws://localhost:${localPort}/runtime/session`,
 
   appId: process.env.NEXT_PUBLIC_INSTANT_APP_ID!,
   devtool: true,
