0c14e2b1b526d91cabf189a614781d29c5b9405e
Drew Harris <drew@drewh.net>
2026-02-13 10:33:40 -0800

    simplify solid lib/db.ts (remove type date)

--- examples/solidjs-vite-advanced/src/lib/db.ts
+++ examples/solidjs-vite-advanced/src/lib/db.ts
@@ -3,13 +3,10 @@ import schema from "../instant.schema";
 
 export { id, tx };
 
-const useDateObjects = true as const;
-export type UsingDateObjects = typeof useDateObjects;
-
 export const db = init({
   appId: import.meta.env.VITE_INSTANT_APP_ID!,
   schema,
-  useDateObjects,
+  useDateObjects: true,
 });
 
 export const chatRoom = db.room("chat", "lobby");
--- examples/solidjs-vite/src/lib/db.ts
+++ examples/solidjs-vite/src/lib/db.ts
@@ -1,11 +1,8 @@
 import { init } from "@instantdb/solidjs";
 import schema from "../instant.schema";
 
-const useDateObjects = true as const;
-export type UsingDateObjects = typeof useDateObjects;
-
 export const db = init({
   appId: import.meta.env.VITE_INSTANT_APP_ID!,
   schema,
-  useDateObjects,
+  useDateObjects: true,
 });
