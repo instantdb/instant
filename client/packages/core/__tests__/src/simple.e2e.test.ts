5628a89ef1fce9430b842b595389446830e5f51e
Drew Harris <drew@drewh.net>
2026-03-04 16:42:53 -0800

    Headless browser sandbox / tests in core package (#2341)

---
+++ client/packages/core/__tests__/src/simple.e2e.test.ts
@@ -0,0 +1,11 @@
+import { expect } from 'vitest';
+import { e2eTest as test } from './utils/e2e';
+
+test('can make a query', async ({ db }) => {
+  const result = await db.queryOnce({ todos: {} });
+
+  document.body.innerHTML = '<h1>Hello</h1>';
+  console.log('Hello World');
+
+  expect(result.data.todos.length).toBe(0);
+});
---
+++ client/packages/core/vitest.config.ts
@@ -0,0 +1,29 @@
+import { playwright } from '@vitest/browser-playwright';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: 'e2e',
+          include: ['**/**.e2e.test.ts'],
+          browser: {
+            enabled: true,
+            provider: playwright(),
+            headless: true,
+            instances: [{ browser: 'chromium' }],
+          },
+        },
+      },
+      {
+        test: {
+          name: 'node',
+          include: ['**/**.test.ts'],
+          exclude: ['**/**.e2e.test.{ts,js}'],
+        },
+      },
+    ],
+  },
+});
