13fe3d43a6b2fc12cdaa67010e7ef13d630b1a2b
Drew Harris <drew@drewh.net>
2026-03-04 16:33:48 -0800

    ignore ts errors in e2e utils

--- client/packages/core/__tests__/src/utils/e2e.ts
+++ client/packages/core/__tests__/src/utils/e2e.ts
@@ -1,13 +1,17 @@
 import { test as baseTest } from 'vitest';
 import { init, InstantCoreDatabase } from '../../../src';
 
+// @ts-ignore
 const apiUrl = import.meta.env.VITE_INSTANT_DEV
   ? 'http://localhost:8888'
-  : import.meta.env.VITE_INSTANT_API_URL || 'https://api.instantdb.com';
+  : // @ts-ignore
+    import.meta.env.VITE_INSTANT_API_URL || 'https://api.instantdb.com';
 
+// @ts-ignore
 const websocketURI = import.meta.env.VITE_INSTANT_DEV
   ? 'ws://localhost:8888/runtime/session'
-  : import.meta.env.VITE_INSTANT_WEBSOCKET_URI ||
+  : // @ts-ignore
+    import.meta.env.VITE_INSTANT_WEBSOCKET_URI ||
     'wss://api.instantdb.com/runtime/session';
 
 export const e2eTest = baseTest.extend<{
