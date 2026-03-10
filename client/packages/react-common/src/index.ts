fa0d74a94d41d021962ae8ce16f62564bafcdf03
Daniel Woelfel <dwwoelfel@gmail.com>
2026-03-10 10:14:07 -0700

    format fix

--- client/packages/react-common/src/index.ts
+++ client/packages/react-common/src/index.ts
@@ -2,5 +2,8 @@ import InstantReactAbstractDatabase from './InstantReactAbstractDatabase.tsx';
 import type { InstantReactRoom } from './InstantReactRoom.ts';
 import { useQueryInternal } from './useQuery.ts';
 
-
-export { InstantReactAbstractDatabase, type InstantReactRoom, useQueryInternal };
+export {
+  InstantReactAbstractDatabase,
+  type InstantReactRoom,
+  useQueryInternal,
+};
--- client/packages/react-common/src/useQuery.ts
+++ client/packages/react-common/src/useQuery.ts
@@ -45,7 +45,7 @@ export function useQueryInternal<
     _query = { $$ruleParams: _opts['ruleParams'], ..._query };
   }
   const query = _query ? coerceQuery(_query) : null;
-  const queryHash = weakHash(query);  
+  const queryHash = weakHash(query);
 
   // We use a ref to store the result of the query.
   // This is becuase `useSyncExternalStore` uses `Object.is`
