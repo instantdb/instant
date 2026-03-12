34a242352c8e88ae768f9b835b57080fc9c2c504
stopachka <stepan.p@gmail.com>
2026-03-12 11:04:35 -0700

    sm

--- client/www/components/new-landing/typography.tsx
+++ client/www/components/new-landing/typography.tsx
@@ -5,12 +5,12 @@ interface TypographyProps {
 }
 
 export function HeroTitle({ children }: TypographyProps) {
-  return <h1 className="text-3xl font-semibold sm:text-5xl">{children}</h1>;
+  return <h1 className="text-3xl font-normal sm:text-5xl">{children}</h1>;
 }
 
 export function SectionTitle({ children }: TypographyProps) {
   return (
-    <h2 className="text-3xl leading-snug font-semibold sm:text-5xl">
+    <h2 className="text-3xl leading-snug font-normal sm:text-5xl">
       {children}
     </h2>
   );
@@ -24,14 +24,14 @@ export function SectionSubtitle({ children }: TypographyProps) {
   );
 }
 
-export function Subheading({ children }: { children: ReactNode }) {
+export function Subheading({ children }: TypographyProps) {
   return <h3 className="text-2xl font-normal sm:text-3xl">{children}</h3>;
 }
 
-export function FeatureBody({ children }: { children: ReactNode }) {
+export function FeatureBody({ children }: TypographyProps) {
   return <p className="mt-2 text-lg">{children}</p>;
 }
 
-export function SectionIntro({ children }: { children: ReactNode }) {
+export function SectionIntro({ children }: TypographyProps) {
   return <div className="sm:text-center">{children}</div>;
 }
