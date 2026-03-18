452e34c7f50a495cbd22657220c51c798c9603cd
stopachka <stepan.p@gmail.com>
2026-03-18 10:09:55 -0700

    roar

--- client/www/components/new-landing/FirebaseTestimonial.tsx
+++ client/www/components/new-landing/FirebaseTestimonial.tsx
@@ -12,10 +12,9 @@ export function FirebaseTestimonial() {
       />
       <div>
         <blockquote className="text-base leading-relaxed text-gray-500 max-sm:text-center sm:text-lg">
-          &ldquo;Instant nails the quad-fecta: offline, real-time, relational
-          queries, and open source. At Firebase, the demand for relational
-          queries was off the charts &mdash; it&rsquo;s a genuinely hard
-          engineering problem. This is a fake testimonial&rdquo;
+          The amount of requests we had for relational queries for Firebase was
+          off-the-charts. I always wanted this built and open sourced. I’m glad
+          to see Instant is doing it!
         </blockquote>
         <div className="mt-4 max-sm:text-center">
           <div className="text-sm font-semibold">James Tamplin</div>
