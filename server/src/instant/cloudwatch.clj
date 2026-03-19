913644c5051f59afb8f1ce04b39cb1675b5a6c30
Daniel Woelfel <dwwoelfel@gmail.com>
2026-03-19 10:24:43 -0700

    better calculation

--- server/src/instant/cloudwatch.clj
+++ server/src/instant/cloudwatch.clj
@@ -12,13 +12,14 @@
    (software.amazon.awssdk.services.rds RdsClient)
    (software.amazon.awssdk.services.rds.model DBInstance DescribeDbInstancesRequest)))
 
-;; XXX: Check that we've got the permissions here
 (defonce cloudwatch-client (delay (CloudWatchClient/create)))
 
 (defonce rds-client (delay (-> (RdsClient/builder)
                                (.build))))
 
-(defn all-rds-instance-ids []
+(defn all-rds-instance-ids
+  "Returns all resource ids for all database instances in RDS."
+  []
   (let [^RdsClient client @rds-client
         ^DescribeDbInstancesRequest req (-> (DescribeDbInstancesRequest/builder) .build)
         response (.describeDBInstances client req)]
@@ -27,22 +28,23 @@
          (mapv (fn [^DBInstance i]
                  (.dbiResourceId i))))))
 
-(defn total-db-queries-for-resources [resource-ids]
+(defn total-db-queries-for-resources
+  "Returns the queries per second over the last minute across all database
+   instances in RDS."
+  [resource-ids]
   (let [^CloudWatchClient client @cloudwatch-client
         now (Instant/now)
-        start (.minus now 2 ChronoUnit/MINUTES)
-
+        start (.minus now 5 ChronoUnit/MINUTES)
+        period (int 60)
         queries (mapcat (fn [resource-id]
                           (let [m-id (str "m_" (crypt-util/random-hex 8))
 
                                 m (-> (MetricDataQuery/builder)
                                       (.id m-id)
-                                      (.expression (format "DB_PERF_INSIGHTS('RDS', '%s',
-                                                            ['db.SQL.queries_started.avg',
-                                                             'db.Transactions.xact_commit.avg',
-                                                             'db.Transactions.xact_rollback.avg'])"
+                                      (.expression (format "DB_PERF_INSIGHTS('RDS', '%s',['db.SQL.queries_started.sum'])"
                                                            resource-id))
-                                      (.period (int 60))
+
+                                      (.period period)
                                       (.returnData false)
                                       (.build))
                                 total (-> (MetricDataQuery/builder)
@@ -62,15 +64,19 @@
 
         ^GetMetricDataResponse response (.getMetricData client ^GetMetricDataRequest request)
         ^MetricDataResult result (.metricDataResults response)]
-    (int (reduce (fn [acc ^MetricDataResult result]
-                   (+ acc (first (.values result))))
-                 0
-                 result))))
+    (int (/ (reduce (fn [acc ^MetricDataResult result]
+                      (+ acc (first (.values result))))
+                    0
+                    result)
+            period))))
 
 (def query-cache (atom nil))
 (def cache-millis (* 1000 60))
 
-(defn total-queries-cached []
+(defn total-queries-cached
+  "Gets the total queries per second over the the last minute across all databases.
+   Caches response for 1 minute."
+  []
   (let [[cache-time cache-data] @query-cache
         now (.toEpochMilli (Instant/now))]
     (if (and cache-time
