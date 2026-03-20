692be4551a9a3fc2d94db23355d01869219b2413
Daniel Woelfel <dwwoelfel@gmail.com>
2026-03-19 17:14:09 -0700

    cleanup whitespace

--- client/www/lib/product/database/examples.ts
+++ client/www/lib/product/database/examples.ts
@@ -14,9 +14,6 @@ export const instantArch = `             With Instant
 ║        ║◀────────────────║         ║
 ╚════════╝  realtime data  ╚═════════╝`;
 
-
-
-
 export const queryExamples = [
   {
     label: 'Nested query',
