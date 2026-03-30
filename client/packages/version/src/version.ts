ffc55166aa5d554605f8329129283dd004d38fab
Daniel Woelfel <dwwoelfel@gmail.com>
2026-03-30 12:43:27 -0700

    Fix merge conflict

--- client/packages/version/src/version.ts
+++ client/packages/version/src/version.ts
@@ -2,10 +2,6 @@
 // Update the version here and merge your code to main to
 // publish a new version of all of the packages to npm.
 
-<<<<<<< next-public-instant-app-id
 const version = 'v0.22.176';
-=======
-const version = 'v0.22.175';
->>>>>>> main
 
 export { version };
