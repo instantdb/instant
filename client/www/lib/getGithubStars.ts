2ece9da2c0329b306451d46e77c64b718b2a7114
Daniel Woelfel <dwwoelfel@gmail.com>
2026-04-02 16:53:55 -0700

    Handle errors fetching stargazers (#2473)

--- client/www/lib/getGithubStars.ts
+++ client/www/lib/getGithubStars.ts
@@ -4,30 +4,37 @@ const githubStarResponseSchema = z.object({
   stargazers_count: z.number(),
 });
 
+const BACKUP_STARGAZERS = 9800;
+
 export const getGithubStarCount = async (): Promise<number> => {
   const accessToken = process.env.GITHUB_PERSONAL_ACCESS_TOKEN;
 
-  const response = await fetch(
-    'https://api.github.com/repos/instantdb/instant',
-    {
-      headers: {
-        Accept: 'application/vnd.github+json',
-        Authorization: accessToken ? `Bearer ${accessToken}` : '',
+  try {
+    const response = await fetch(
+      'https://api.github.com/repos/instantdb/instant',
+      {
+        headers: {
+          Accept: 'application/vnd.github+json',
+          Authorization: accessToken ? `Bearer ${accessToken}` : '',
+        },
+        next: {
+          revalidate: 3600,
+        },
       },
-      next: {
-        revalidate: 3600,
-      },
-    },
-  );
-  if (!response.ok) {
-    throw new Error(await response.text());
-  }
-  const data = await response.json();
-  const parseResult = githubStarResponseSchema.safeParse(data);
+    );
+    if (!response.ok) {
+      throw new Error(await response.text());
+    }
+    const data = await response.json();
+    const parseResult = githubStarResponseSchema.safeParse(data);
 
-  if (parseResult.error) {
-    throw new Error(parseResult.error.message);
-  }
+    if (parseResult.error) {
+      throw new Error(parseResult.error.message);
+    }
 
-  return parseResult.data.stargazers_count;
+    return parseResult.data.stargazers_count;
+  } catch (e) {
+    console.error('Could not fetch stargazer count', e);
+    return BACKUP_STARGAZERS;
+  }
 };
