96c725d2c7e3994c3dc52480fa1ffdd3572b731d
Stepan Parunashvili <stepan.p@gmail.com>
2026-04-06 10:30:23 -0700

    Small landing page fixes (#2487)

--- client/www/tailwind.config.js
+++ client/www/tailwind.config.js
@@ -29,7 +29,7 @@ module.exports = {
     fontFamily: {
       sans: [...sans],
       mono: [
-        'Berk Mono',
+        'Berkeley Mono',
         'ui-monospace',
         'SFMono-Regular',
         'Menlo',
