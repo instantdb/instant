52f17b6a84632f05dc0faca1a08425461cc3275b
Drew Harris <drew@drewh.net>
2026-04-16 15:46:36 -0700

    bump version to v1.0.7

--- client/packages/version/src/version.ts
+++ client/packages/version/src/version.ts
@@ -2,6 +2,6 @@
 // Update the version here and merge your code to main to
 // publish a new version of all of the packages to npm.
 
-const version = 'v1.0.6';
+const version = 'v1.0.7';
 
 export { version };
