cf9d0bd980ae57b084ddc0d36e22858fe30a6574
Daniel Woelfel <dwwoelfel@gmail.com>
2026-04-20 09:47:28 -0700

    router.replace

--- client/www/app/logout/page.tsx
+++ client/www/app/logout/page.tsx
@@ -1,14 +1,17 @@
 'use client';
 
 import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import { signOut } from '@/lib/auth';
 
 export default function LogoutPage() {
+  const router = useRouter();
+
   useEffect(() => {
     signOut().finally(() => {
-      window.location.replace('/');
+      router.replace('/');
     });
-  }, []);
+  }, [router]);
 
   return null;
 }
