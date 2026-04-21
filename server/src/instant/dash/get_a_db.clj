ef029b8cdce116e492794c6f7b0ee9ef106ff872
stopachka <stepan.p@gmail.com>
2026-04-21 16:09:08 -0700

    roar

--- server/src/instant/dash/get_a_db.clj
+++ server/src/instant/dash/get_a_db.clj
@@ -69,10 +69,9 @@
 (comment
   ;; Need to generate a personal access token? 
   ;; This is useful for testing cient/www/getadb in development 
-  (tool/with-prod-conn [conn]
-    (instant-personal-access-token-model/create!
-     conn
-     {:user-id (:id @get-a-db-creator) :name "getadb-pat"})))
+  (instant-personal-access-token-model/create!
+    conn
+    {:user-id (:id @get-a-db-creator) :name "getadb-pat"}))
 
 (comment
   (def res (http-post-handler {:body {:title "my-app"}}))
