9540bf3affedee75ce727c0637b815897f3afd86
stopachka <stepan.p@gmail.com>
2026-04-21 09:52:47 -0700

    seed

---
+++ server/resources/migrations/105_seed_get_a_db_app_creator.up.sql
@@ -0,0 +1,15 @@
+insert into
+  instant_users (id, email, created_at, google_sub)
+values
+  (
+    '9bacaf3d-0c7d-46e6-bb1b-4b9fdb9c5b61',
+    'hello+getadbapps@instantdb.com',
+    '2026-04-21 00:00:00',
+    null
+  ),
+  (
+    '9bacaf3d-0c7d-46e6-bb1b-4b9fdb9c5b61',
+    'hello+getadbapps@instantdb.com',
+    '2026-04-21 00:00:00',
+    null
+  ) on conflict do nothing;
