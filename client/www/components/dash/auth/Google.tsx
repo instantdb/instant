6ca4780f995544736d5fc91f401073c0c7bb5e0c
stopachka <stepan.p@gmail.com>
2026-04-24 11:26:35 -0700

    Always set skipNonceChecks when creating Google OAuth client

--- client/www/components/dash/auth/Google.tsx
+++ client/www/components/dash/auth/Google.tsx
@@ -126,7 +126,7 @@ export function AddGoogleClientForm({
         redirectTo: useSharedCredentials ? undefined : redirectTo,
         useSharedCredentials,
         meta: {
-          skipNonceChecks: isNative(appType),
+          skipNonceChecks: true,
           appType,
         },
       });
