0d3694247f66abea254d37b0387b5bea7319b372
Drew Harris <drew@drewh.net>
2026-05-01 13:38:37 -0700

    remove deployment route

--- client/www/app/api/deployment/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-
-export const GET = async (req: NextRequest): Promise<NextResponse> => {
-  return NextResponse.json({
-    hello: 'there',
-  });
-};
