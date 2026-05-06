a0c88e790f9a4cb95d7dfaf5c5e7b984feb8e6d9
stopachka <stepan.p@gmail.com>
2026-05-06 15:31:42 -0700

    [getadb] use www. host in provision URL

--- client/www/app/getadb/guideMarkdown.ts
+++ client/www/app/getadb/guideMarkdown.ts
@@ -2,7 +2,7 @@ export const PLACEHOLDER_TOKEN = '<generate-a-uuid>';
 
 export const GUIDE_MARKDOWN = `This is Instant's get-a-db service.
 
-To provision credentials, fetch https://getadb.com/provision/${PLACEHOLDER_TOKEN}.
+To provision credentials, fetch https://www.getadb.com/provision/${PLACEHOLDER_TOKEN}.
 
 Generate a random UUID yourself and use a different UUID each time. The
 unique URL prevents upstream caches (e.g. WebFetch's 15-minute URL cache)
