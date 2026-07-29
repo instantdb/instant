eec65541b407d4cbe94ceb0f7660bb76114db8ce
Daniel Woelfel <dwwoelfel@gmail.com>
2026-07-29 11:21:16 -0700

    include app_id in replica identity

---
+++ server/resources/migrations/119_replica_identity.down.sql
@@ -0,0 +1,6 @@
+alter table idents replica identity default;
+alter table attr_sketches replica identity default;
+alter table app_admin_tokens replica identity default;
+alter table app_email_templates replica identity default;
+alter table app_email_verifications replica identity default;
+alter table app_email_senders replica identity default;
