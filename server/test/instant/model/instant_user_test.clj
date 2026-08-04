5fe8a288cbd1045e89674c7bfb4f7d3d16bf9e2f
Joe Averbukh <joeaverbukh@gmail.com>
2026-08-03 17:15:24 -0700

    [Self-host] Prevent system users from logging in to self-hosted deployments

--- server/test/instant/model/instant_user_test.clj
+++ server/test/instant/model/instant_user_test.clj
@@ -4,7 +4,20 @@
    [instant.fixtures :refer [with-empty-app with-org with-user]]
    [instant.jdbc.aurora :as aurora]
    [instant.jdbc.sql :as sql]
-   [instant.model.instant-user :as instant-user]))
+   [instant.model.instant-user :as instant-user]
+   [instant.model.instant-user-refresh-token :as instant-user-refresh-token]))
+
+(deftest disabled-dashboard-login-prevents-token-creation
+  (with-user
+    (fn [user]
+      (sql/execute-one!
+       (aurora/conn-pool :write)
+       ["INSERT INTO user_flags (id, user_id, flag_name)
+         VALUES (?::uuid, ?::uuid, 'dashboard-login-disabled')"
+        (random-uuid) (:id user)])
+      (is (thrown? Exception
+                   (instant-user-refresh-token/create!
+                    {:id (random-uuid) :user-id (:id user)}))))))
 
 (deftest get-by-app-id
   (with-user
