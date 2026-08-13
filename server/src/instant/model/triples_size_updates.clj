4b99cca678e51b6f6fcc54fbe1a6972863478272
Daniel Woelfel <dwwoelfel@gmail.com>
2026-08-12 21:31:47 -0700

    count the work done when processing batches

--- server/src/instant/model/triples_size_updates.clj
+++ server/src/instant/model/triples_size_updates.clj
@@ -23,34 +23,39 @@
            [:deletes {:delete-from :triples-size-updates
                       :using :ids
                       :where [:= :triples-size-updates.id :ids.id]
-                      :returning [:app-id :attr-id :pg-size :files-size]}]]
+                      :returning [:app-id :attr-id :pg-size :files-size]}]
+           [:aggregate {:insert-into [[:triples_size_aggregate [:app-id :attr-id :pg-size :files-size]]
+                                      {:select [:deletes.app-id :deletes.attr-id [[:sum :deletes.pg_size] :pg-size] [[:sum :deletes.files-size] :files-size]]
+                                       :from :deletes
+                                       ;; Join filters out (app_id, attr_id) whose parent was deleted mid-batch.
+                                       :join [:apps [:= :apps.id  :deletes.app-id]
+                                              :attrs [:= :attrs.id :deletes.attr-id]]
+                                       :group-by [:deletes.app-id :deletes.attr-id]
+                                       ;; sort to avoid deadlock
+                                       :order-by [:deletes.app-id :deletes.attr-id]}]
+                        :on-conflict {:on-constraint :triples_size_aggregate_pkey}
+                        :do-update-set {:pg-size [:+
+                                                  :triples_size_aggregate.pg_size
+                                                  :excluded.pg_size]
+                                        ;; Update files-size only if one of the arguments is non-null
+                                        :files-size [:coalesce
+                                                     [:+
+                                                      :triples_size_aggregate.files_size
+                                                      :excluded.files_size]
+                                                     :triples_size_aggregate.files_size
+                                                     :excluded.files_size]}
+                        ;; RETURNING makes this CTE referenceable below, which
+                        ;; forces Postgres to execute it.
+                        :returning [:1]}]]
+    :select [[{:select [[[:count :*]]] :from :deletes} :deleted-count]
+             [{:select [[[:count :*]]] :from :aggregate} :aggregated-count]]}))
 
-    :insert-into [[:triples_size_aggregate [:app-id :attr-id :pg-size :files-size]]
-                  {:select [:deletes.app-id :deletes.attr-id [[:sum :deletes.pg_size] :pg-size] [[:sum :deletes.files-size] :files-size]]
-                   :from :deletes
-                   ;; Join filters out (app_id, attr_id) whose parent was deleted mid-batch.
-                   :join [:apps [:= :apps.id  :deletes.app-id]
-                          :attrs [:= :attrs.id :deletes.attr-id]]
-                   :group-by [:deletes.app-id :deletes.attr-id]
-                   ;; sort to avoid deadlock
-                   :order-by [:deletes.app-id :deletes.attr-id]}]
-    :on-conflict {:on-constraint :triples_size_aggregate_pkey}
-    :do-update-set {:pg-size [:+
-                              :triples_size_aggregate.pg_size
-                              :excluded.pg_size]
-                    ;; Update files-size only if one of the arguments is non-null
-                    :files-size [:coalesce
-                                 [:+
-                                  :triples_size_aggregate.files_size
-                                  :excluded.files_size]
-                                 :triples_size_aggregate.files_size
-                                 :excluded.files_size]}}))
 (defn collect-batch!
   ([] (collect-batch! (aurora/conn-pool :write) (flags/triples-size-collection-batch-size)))
   ([conn batch-size]
-   (sql/do-execute! ::collect-batch!
-                    conn
-                    (uhsql/formatp collect-batch-q {:batch-size batch-size}))))
+   (sql/execute-one! ::collect-batch!
+                     conn
+                     (uhsql/formatp collect-batch-q {:batch-size batch-size}))))
 
 (defn collect-batches!
   "Adds triples_size_updates to the triples_size_aggregates table and deletes them.
@@ -64,10 +69,12 @@
    (tracer/with-span! {:name ::collect-batches
                        :attributes {:max-loops max-loops}}
      (loop [loops 0
-            total-collected 0]
+            total-collected 0
+            total-aggregated 0]
        (if (= loops max-loops)
          (let [hits (swap! consecutive-limit-hits inc)]
            (tracer/add-data! {:attributes {:total-collected total-collected
+                                           :total-aggregated total-aggregated
                                            :loops loops
                                            :consecutive-limit-hits hits}})
            (when (and (config/prod?)
@@ -75,14 +82,18 @@
              (discord/send-error-async! (str (:instateam discord/mention-constants)
                                              " collect triples size is backed up after " loops " iterations"
                                              " (" hits " runs in a row)."))))
-         (let [update-count (:next.jdbc/update-count (first (collect-batch!)))]
-           (if (zero? update-count)
+         (let [{:keys [deleted_count aggregated_count]} (collect-batch!)]
+           ;; Drain on queue rows removed, not aggregate rows written: an
+           ;; orphan-only batch deletes rows but writes 0 aggregates.
+           (if (zero? (long deleted_count))
              (do
                (reset! consecutive-limit-hits 0)
                (tracer/add-data! {:attributes {:total-collected total-collected
+                                               :total-aggregated total-aggregated
                                                :loops loops}}))
              (recur (inc loops)
-                    (+ total-collected (long update-count))))))))))
+                    (+ total-collected (long deleted_count))
+                    (+ total-aggregated (long aggregated_count))))))))))
 
 (defn start []
   (let [consecutive-limit-hits (atom 0)
